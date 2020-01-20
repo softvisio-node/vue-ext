72015793abef871dd6c7e470bc0330e0847fac18
zdm <zdm@softvisio.net>
2020-01-20 16:02:32 +0200

    amcharts4 component added

---
+++ lib/am4charts-panel.vue
@@ -0,0 +1,140 @@
+<!--
+# USAGE
+
+yarn add @amcharts/amcharts4
+
+import AmchartsPanel from "#swc/am4charts-panel";
+
+<AmchartsPanel animated="false" @ready="chartsReady"/>
+
+chartsReady (chart) {
+    chart.create( amChartsConfig );
+
+    chart.setStore(...);
+
+    chart.dataHandler = function ( data ) { return data };
+
+    chart.setData(...);
+}
+
+-->
+
+<template>
+    <ext-panel layout="fit" @ready="_ready"/>
+</template>
+
+<script>
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import chartThemeAnimated from "@amcharts/amcharts4/themes/animated";
+import chartThemeLight from "@amcharts/amcharts4/themes/material";
+import chartThemeDark from "@amcharts/amcharts4/themes/amchartsdark";
+
+am4core.options.commercialLicense = true;
+
+export default {
+    "props": {
+        "animated": {
+            "type": Boolean,
+            "default": true,
+        },
+    },
+
+    "computed": {
+        darkMode () {
+            return this.$store.getters["session/darkMode"];
+        },
+    },
+
+    mounted () {
+        this.$once( "hook:beforeDestroy", () => {
+            // destroy chart
+            if ( this.chart ) this.chart.dispose();
+            this.chart = null;
+
+            // destroy ext component
+            if ( this.cmp ) this.cmp.destroy();
+            this.cmp = null;
+        } );
+    },
+
+    "methods": {
+        _ready ( e ) {
+            this.cmp = e.detail.cmp;
+
+            this.cmp.afterRender = this._afterRender.bind( this );
+        },
+
+        _afterRender () {
+            this.cmp.afterRender = null;
+
+            this.$watch( "darkMode", this._onDarkModeChange.bind( this ) );
+
+            this.$emit( "ready", this );
+        },
+
+        _onDarkModeChange () {
+            this.chart.dispose();
+
+            this.chart = null;
+
+            this.$emit( "ready", this );
+        },
+
+        create ( config ) {
+            // theme
+            am4core.unuseAllThemes();
+            if ( this.animated ) am4core.useTheme( chartThemeAnimated );
+            am4core.useTheme( this.darkMode ? chartThemeDark : chartThemeLight );
+
+            this.chart = am4core.createFromConfig( JSON.parse( JSON.stringify( config ) ), this.cmp.innerElement.dom );
+        },
+
+        setData ( data ) {
+            this.chart.data = data;
+        },
+
+        setStore ( store ) {
+            var oldStore = this.store,
+                bindEvents = Ext.apply( {
+                    "scope": this,
+                },
+                {
+                    "dataChanged": this._onStoreDataChanged,
+                } );
+
+            if ( oldStore ) {
+                oldStore.un( bindEvents );
+            }
+
+            this.store = store;
+
+            store.on( bindEvents );
+
+            this._onStoreDataChanged();
+        },
+
+        _onStoreDataChanged () {
+            const data = Ext.Array.pluck( this.store.data.items, "data" );
+
+            const dataHandler = this.dataHandler;
+
+            if ( dataHandler ) {
+                this.chart.data = dataHandler( data );
+            }
+            else {
+                this.chart.data = data;
+            }
+        },
+    },
+};
+</script>
+<!-- -----SOURCE FILTER LOG BEGIN----- -->
+<!-- -->
+<!-- +-------+---------------+------------------------------+--------------------------------------------------------------------------------+ -->
+<!-- | Sev.  | Line:Col      | Rule                         | Description                                                                    | -->
+<!-- |=======+===============+==============================+================================================================================| -->
+<!-- |  WARN | 28:13         | no-unused-vars               | 'am4charts' is defined but never used.                                         | -->
+<!-- +-------+---------------+------------------------------+--------------------------------------------------------------------------------+ -->
+<!-- -->
+<!-- -----SOURCE FILTER LOG END----- -->
