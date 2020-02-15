82fc630d4837ef50cd0fdb30bfd074ccec67e1fe
zdm <zdm@softvisio.net>
2020-02-15 17:18:35 +0200

    pdf view updated

--- lib/pdf-panel.vue
+++ lib/pdf-panel.vue
@@ -5,14 +5,18 @@ yarn add worker-loader pdfjs-dist
 
 Events:
 ready(this);
+
+https://rossta.net/blog/building-a-pdf-viewer-with-vue-part-1.html
 -->
 
 <template>
-    <ext-panel layout="center" scrollable="true" @ready="ready"/>
+    <ext-container layout="center" scrollable="true" @ready="ready"/>
 </template>
 
 <script>
 import pdfjs from "pdfjs-dist/webpack";
+
+const pixelRatio = window.devicePixelRatio || 1;
 
 export default {
     "props": {
@@ -24,19 +28,62 @@ export default {
             "type": String,
             "default": "auto",
         },
+        "resetZoomOnLoad": {
+            "type": Boolean,
+            "default": true,
+        },
+        "zoomStep": {
+            "type": Number,
+            "default": 0.1,
+        },
+        "minZoom": {
+            "type": Number,
+            "default": 0.5,
+        },
+        "maxZoom": {
+            "type": Number,
+            "default": 2,
+        },
     },
 
     data () {
         return {
             "cmp": null,
-            "pdf": null,
-            "lastSrc": null,
+
+            "currentSrc": null,
+            "currentZoom": 1,
+
+            "pdfDoc": null,
+            "pdfPages": [],
         };
+    },
+
+    "watch": {
+        "src": "setSrc",
     },
 
     "methods": {
         ready ( e ) {
             this.cmp = e.detail.cmp;
+
+            Ext.widget( "container", {
+                "floating": true,
+                "renderTo": this.cmp,
+                "style": "position: absolute; bottom: 20px; right: 20px;",
+                "layout": {
+                    "type": "vbox",
+                },
+
+                "items": [
+                    { "xtype": "button", "iconCls": "fas fa-search-plus", "ui": "action", "handler": this.zoomIn.bind( this, null ) },
+                    { "xtype": "container", "height": "10" },
+                    { "xtype": "button", "iconCls": "fas fa-arrows-alt", "ui": "action", "handler": this.resetZoom.bind( this ) },
+                    { "xtype": "container", "height": "10" },
+                    { "xtype": "button", "iconCls": "fas fa-search-minus", "ui": "action", "handler": this.zoomOut.bind( this, null ) },
+                ],
+            } );
+
+            if ( this.src ) this.setSrc( this.src );
 
             if ( this.cmp.rendered ) {
                 this._afterRender();
@@ -46,24 +93,58 @@ export default {
             }
         },
 
-        _afterRender () {
+        async _afterRender () {
             this.cmp.afterRender = null;
 
-            this.cmp.el.on( {
-                "scope": this,
-                "buffer": 300,
-                "pinch": this.onPinch,
-            } );
+            // this.cmp.el.on( {
+            //     "scope": this,
+            //     "buffer": 300,
+            //     "pinch": this._onPinch,
+            // } );
 
-            this.$watch( "src", this._load.bind( this ) );
+            await this._renderPdfDoc();
 
             this.$emit( "ready", this );
-
-            if ( this.src ) this.setSrc( this.src );
         },
 
+        // zoom
+        zoomIn ( zoomStep ) {
+            var newZoom = this.currentZoom + ( zoomStep || this.zoomStep );
+
+            this.setZoom( newZoom );
+        },
+
+        zoomOut ( zoomStep ) {
+            var newZoom = this.currentZoom - ( zoomStep || this.zoomStep );
+
+            this.setZoom( newZoom );
+        },
+
+        resetZoom () {
+            this.setZoom( 1 );
+        },
+
+        setZoom ( newZoom ) {
+            if ( newZoom === this.currentZoom ) {
+                return;
+            }
+            else if ( newZoom < this.minZoom ) {
+                newZoom = this.minZoom;
+            }
+            else if ( newZoom > this.maxZoom ) {
+                newZoom = this.maxZoom;
+            }
+
+            if ( newZoom !== this.currentZoom ) {
+                this.currentZoom = newZoom;
+
+                this._scale();
+            }
+        },
+
+        // src
         getSrc () {
-            return this.lastSrc;
+            return this.currentSrc;
         },
 
         setSrc ( src ) {
@@ -71,94 +152,154 @@ export default {
                 this.clear();
             }
             else {
-                this.lastSrc = src;
+                this.currentSrc = src;
 
-                this._load();
+                this._renderPdfDoc();
             }
         },
 
         clear () {
             this.cmp.setHtml( "" );
 
-            this.pdf = null;
+            this.pdfDoc = null;
+            this.pdfPages = [];
 
-            this.lastSrc = null;
+            this.currentSrc = null;
+            this.currentZoom = 1;
         },
 
         reload () {
-            this._load();
+            this._renderPdfDoc();
         },
 
-        async _load () {
-            if ( !this.lastSrc ) return;
+        async _renderPdfDoc () {
+            if ( !this.cmp.rendered ) return;
 
-            var me = this,
-                cmp = this.cmp;
+            if ( !this.currentSrc ) return;
+
+            if ( this.resetZoomOnLoad ) this.currentZoom = 1;
+
+            var cmp = this.cmp;
 
             cmp.mask();
 
-            const loadingTask = pdfjs.getDocument( this.lastSrc );
+            const loadingTask = pdfjs.getDocument( this.currentSrc );
 
             // loadingTask.onProgress = function ( progress ) {
             //     var percent = parseInt( ( progress.loaded / progress.total ) * 100 );
             // };
 
-            const pdf = await loadingTask.promise;
+            const pdfDoc = await loadingTask.promise;
 
-            const numPages = pdf.numPages,
+            const numPages = pdfDoc.numPages,
                 container = document.createElement( "div" );
 
             cmp.setHtml( "" );
-            me.pdf = pdf;
+            this.pdfDoc = pdfDoc;
 
-            container.setAttribute( "style", "display:flex;flex-direction:column;justify-content:flex-start;text-align:center;width:100%;" );
-
-            // el.appendChild( container );
+            // cmp.el.appendChild( container );
             cmp.setHtml( container );
 
-            var parentWidth = cmp.element.getWidth() - 30;
-            // var parentHeight = cmp.element.getHeight();
+            container.style = "display:flex;flex-direction:column;justify-content:flex-start;text-align:center;width:100%;height:100%;";
 
             for ( var i = 1; i <= numPages; i++ ) {
                 // fetch page
-                const page = await pdf.getPage( i );
+                const page = await pdfDoc.getPage( i );
 
-                const pageViewport = page.getViewport( { "scale": 1 } );
-
-                let pageScale = me.scale;
-
-                if ( pageScale === "auto" ) {
-                    pageScale = parentWidth / pageViewport.width;
-                }
-
-                var viewport = page.getViewport( { "scale": pageScale } );
-
-                // Prepare canvas using PDF page dimensions
-                var div = document.createElement( "div" );
-                container.appendChild( div );
-
-                var canvas = document.createElement( "canvas" );
-                div.appendChild( canvas );
-
-                var context = canvas.getContext( "2d" );
-                canvas.height = viewport.height;
-                canvas.width = viewport.width;
-
-                // Render PDF page into canvas context
-                var renderContext = {
-                    "canvasContext": context,
-                    "viewport": viewport,
-                };
-
-                page.render( renderContext );
+                await this._renderPdfPage( container, page );
             }
 
             cmp.unmask();
         },
 
-        onPinch ( e ) {
-            console.log( e );
+        async _renderPdfPage ( container, page ) {
+            var cmp = this.cmp,
+                scale = this.scale,
+                viewport;
+
+            if ( scale === "auto" ) {
+                var parentWidth = cmp.element.getWidth() - 30;
+                // var parentHeight = cmp.element.getHeight();
+
+                viewport = page.getViewport( { "scale": 1 } );
+
+                scale = ( parentWidth / viewport.width ) * pixelRatio;
+
+                viewport = viewport.clone( { "scale": scale } );
+            }
+            else {
+                scale = scale * pixelRatio;
+
+                viewport = page.getViewport( { "scale": scale } );
+            }
+
+            // create canvas
+            var canvas = document.createElement( "canvas" );
+
+            var context = canvas.getContext( "2d" );
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+
+            canvas.style = this._getCanvasStyle( viewport, scale );
+
+            container.appendChild( canvas );
+
+            // Render PDF page into canvas context
+            var renderContext = {
+                "canvasContext": context,
+                "viewport": viewport,
+            };
+
+            // var renderTask =
+            await page.render( renderContext ).promise;
+
+            this.pdfPages.push( {
+                "canvas": canvas,
+                "viewport": viewport,
+                "scale": scale,
+            } );
         },
+
+        _getCanvasStyle ( viewport, scale ) {
+            var actualSizeViewport = viewport.clone( { "scale": scale } );
+
+            var pixelWidth = Math.ceil( actualSizeViewport.width / pixelRatio );
+            var pixelHeight = Math.ceil( actualSizeViewport.height / pixelRatio );
+
+            var style = `width: ${pixelWidth}px; height: ${pixelHeight}px;`;
+
+            return style;
+        },
+
+        _scale () {
+            for ( const page of this.pdfPages ) {
+                var style = this._getCanvasStyle( page.viewport, page.scale * this.currentZoom );
+
+                page.canvas.style = style;
+            }
+        },
+
+        // _onPinch ( e ) {
+        //     if ( e.type === "pinch" ) {
+        //         if ( e.scale > 1 ) {
+        //             this.zoomIn( e.scale );
+        //         }
+        //         else {
+        //             this.zoomOut( e.scale );
+        //         }
+        //     }
+
+        //     // distance: 424.1556318145499
+        //     // scale: 3.4347119095550567
+        //     // type: "pinch"
+
+        //     // distance: 113.35784048754634
+        //     // scale: 0.29411071738462125
+        //     // type: "pinch"
+        //     else {
+        //         this.zoomIn( 0.1 );
+        //     }
+        // },
     },
 };
 </script>
