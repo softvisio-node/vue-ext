1e41cc0e71ca94e53f3d2f16976d2f13f7dafad2
zdm <zdm@softvisio.net>
2020-04-18 11:44:27 +0300

    store

--- resources/vue.config.js
+++ resources/vue.config.js
@@ -1,6 +1,11 @@
 const config = require( "@softvisio/vue/resources/vue.config" );
 
+const baseConfigureWebpack = config.configureWebpack;
+
 config.configureWebpack = ( config ) => {
+    // call base method
+    baseConfigureWebpack( config );
+
     // aliases
     config.resolve.alias["#ext.js"] = "@softvisio/ext/lib/ext-" + process.env.EXT_VERSION + ".js";
     config.resolve.alias["#ewc.js"] = "@softvisio/ext/lib/ewc-" + process.env.EWC_VERSION + ".js";
