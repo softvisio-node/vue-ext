4e4c1c5fd331047e491187eff32ad0f8888e265b
zdm <zdm@softvisio.net>
2021-09-11 08:18:14 +0300

    feat!: webpack : aliases renamed to #

--- resources/vue.config.cjs
+++ resources/vue.config.cjs
@@ -10,10 +10,10 @@ config.configureWebpack = config => {
     // aliases
     config.resolve.alias["#softvisio"] = "@softvisio/vue-ext";
 
-    config.resolve.alias[":ext$"] = "@softvisio/ext/ext-" + process.env.EXT_VERSION;
-    config.resolve.alias[":ext-resources"] = "@softvisio/ext/resources/ext-" + process.env.EXT_VERSION;
-    config.resolve.alias[":ewc$"] = "@softvisio/ext/ewc-" + process.env.EWC_VERSION;
-    config.resolve.alias[":ewc-resources"] = "@softvisio/ext/resources/ewc-" + process.env.EWC_VERSION;
+    config.resolve.alias["#ext$"] = "@softvisio/ext/ext-" + process.env.EXT_VERSION;
+    config.resolve.alias["#ext-resources"] = "@softvisio/ext/resources/ext-" + process.env.EXT_VERSION;
+    config.resolve.alias["#ewc$"] = "@softvisio/ext/ewc-" + process.env.EWC_VERSION;
+    config.resolve.alias["#ewc-resources"] = "@softvisio/ext/resources/ewc-" + process.env.EWC_VERSION;
 
     // node polyfills
     config.resolve.fallback ||= {};
