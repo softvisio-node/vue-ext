859e01f09d2dd43ebeeb6cdab4edba6d3dcd03c8
zdm <zdm@softvisio.net>
2021-09-15 10:49:23 +0300

    feat: auto signout dialog

---
+++ lib/components/signout-dialog.vue
@@ -0,0 +1,23 @@
+<template>
+    <ext-dialog title="Server Connection Error" width="250" height="300" closable="false" draggable="false" closeAction="hide" shadow="true">
+        <ext-panel layout="center" html="Unable to connect to the API server."/>
+
+        <ext-toolbar docked="bottom" layout='{"type":"hbox","pack":"center"}'>
+            <ext-button iconCls="fas fa-redo" text="Try Again" ui="action" @tap="close"/>
+        </ext-toolbar>
+    </ext-dialog>
+</template>
+
+<script>
+export default {
+    "emits": ["close"],
+
+    "methods": {
+        close () {
+            this.ext.hide();
+
+            this.$emit( "close" );
+        },
+    },
+};
+</script>
