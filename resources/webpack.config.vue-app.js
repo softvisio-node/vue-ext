4ff7bf5b74d2d7486b03b0f2245b2cb3ffa9be35
zdm <zdm@softvisio.net>
2021-09-30 02:41:27 +0300

    feat: webpack migration

--- resources/webpack.config.vue-app.js
+++ resources/webpack.config.vue-app.js
@@ -1,7 +1,7 @@
 import config from "@softvisio/vue/resources/webpack.config.vue-app.js";
 
-config.resolve.alias["#vue"] = "@softvisio/vue";
+config.resolve.alias["#vue"] = "@softvisio/vue-ext";
 
-config.modules[1].exclude.push( /[\\/]resources[\\/]ext-[\d.]+[\\/]/, /[\\/]resources[\\/]ewc-[\d.]+[\\/]/ );
+config.module.rules[1].exclude.push( /[\\/]resources[\\/]ext-[\d.]+[\\/]/, /[\\/]resources[\\/]ewc-[\d.]+[\\/]/ );
 
 export default config;
