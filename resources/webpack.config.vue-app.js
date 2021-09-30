4b8495f282e3d7c75a047b944188be812f949989
zdm <zdm@softvisio.net>
2021-09-30 13:38:52 +0300

    feat: webpack migration

--- resources/webpack.config.vue-app.js
+++ resources/webpack.config.vue-app.js
@@ -4,4 +4,11 @@ config.resolve.alias["#vue"] = "@softvisio/vue-ext";
 
 // config.module.rules[1].exclude.push( /[\\/]resources[\\/]ext-[\d.]+[\\/]/, /[\\/]resources[\\/]ewc-[\d.]+[\\/]/ );
 
+config.optimization.splitChunks.cacheGroups["ext"] = {
+    "name": "ext",
+    "test": /[\\/]softvisio-ext[\\/]/,
+    "priority": -10,
+    "chunks": "initial",
+};
+
 export default config;
