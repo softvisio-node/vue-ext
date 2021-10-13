86713ac0a9781848a7177cd3d031bb422fe12664
zdm <zdm@softvisio.net>
2021-10-13 15:53:48 +0300

    fix: aliases

--- resources/webpack.config.firebase-worker.js
+++ resources/webpack.config.firebase-worker.js
@@ -2,8 +2,7 @@ import config from "@softvisio/vue/resources/webpack.config.firebase-worker.js";
 
 config.resolve.alias = {
     ...config.resolve.alias,
-    "#vue$": "@softvisio/vue-ext",
-    "#vue/": "@softvisio/vue-ext/",
+    "#vue": "@softvisio/vue-ext",
 };
 
 export default config;
