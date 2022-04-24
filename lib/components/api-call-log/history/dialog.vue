aa3c1de37b4b76e74c9517ca8a3a602eb735b6c5
zdm <zdm@softvisio.net>
2022-04-24 14:31:14 +0300

    fix: amcharts5 migration

--- lib/components/api-call-log/history/dialog.vue
+++ lib/components/api-call-log/history/dialog.vue
@@ -6,19 +6,17 @@
         </ext-toolbar>
 
         <Amcharts5Panel height="250" @ready="_createLoadChart" @data="_updateChart"/>
-
-        <AmchartsPanel height="300" @ready="_runtimeChartReady"/>
-        <AmchartsPanel height="300" @ready="_exceptionsChartReady"/>
+        <Amcharts5Panel height="250" @ready="_createRuntimeChart" @data="_updateChart"/>
+        <Amcharts5Panel height="250" @ready="_createExceptionsChart" @data="_updateChart"/>
     </ext-dialog>
 </template>
 
 <script>
-import AmchartsPanel from "#vue/components/amcharts4/panel";
 import Amcharts5Panel from "#vue/components/amcharts5/panel";
 import * as am5xy from "@amcharts/amcharts5/xy";
 
 export default {
-    "components": { AmchartsPanel, Amcharts5Panel },
+    "components": { Amcharts5Panel },
 
     data () {
         return { "title": "History" };
@@ -124,135 +122,157 @@ export default {
             this.refresh();
         },
 
-        // XXX
-        _runtimeChartReady ( chart ) {
-            this.runtimeChart = chart;
+        _createRuntimeChart ( cmp ) {
+            this._runtimeChart = cmp;
 
-            chart.create( {
-                "titles": [{ "text": "Average Runtime for Last 30 Days", "fontSize": 12 }],
-                "type": "XYChart",
+            const root = cmp.root,
+                am5 = cmp.am5;
 
-                "dateFormatter": { "inputDateFormat": "yyyy-MM-dd HH:mm:ss" },
+            const chart = root.container.children.push( am5xy.XYChart.new( root, {
+                "layout": root.verticalLayout,
+                "panX": true,
+                "panY": true,
+                "pinchZoomX": true,
 
-                "cursor": { "type": "XYCursor", "behavior": "zoomX" },
+                // "wheelX": "panX",
+                // "wheelY": "zoomX",
+            } ) );
 
-                "scrollbarX": {
-                    "type": "XYChartScrollbar",
-                    "series": ["runtime"],
+            chart.set( "cursor",
+                am5xy.XYCursor.new( root, {
+                    "behavior": "zoomX",
+                } ) );
+
+            // add scrollbar, https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+            chart.set( "scrollbarX",
+                am5.Scrollbar.new( root, {
+                    "orientation": "horizontal",
+                } ) );
+
+            chart.children.unshift( am5.Label.new( root, {
+                "text": "Average Runtime for Last 30 Days",
+                "fontSize": 12,
+                "x": am5.percent( 50 ),
+                "centerX": am5.percent( 50 ),
+            } ) );
+
+            const xAxis = chart.xAxes.push( am5xy.DateAxis.new( root, {
+                "maxDeviation": 0,
+                "baseInterval": {
+                    "timeUnit": "hour",
+                    "count": 1,
                 },
+                "renderer": am5xy.AxisRendererX.new( root, {} ),
+                "tooltipDateFormat": "I",
+                "tooltip": am5.Tooltip.new( root, {} ),
+            } ) );
 
-                "xAxes": [
-                    {
-                        "type": "DateAxis",
+            const yAxis = chart.yAxes.push( am5xy.ValueAxis.new( root, {
+                "renderer": am5xy.AxisRendererY.new( root, {} ),
+            } ) );
 
-                        // grouping
-                        "groupData": true,
-                        "groupCount": 24 * 30,
+            const series1 = chart.series.push( am5xy.ColumnSeries.new( root, {
+                "name": "Runtime",
+                "xAxis": xAxis,
+                "yAxis": yAxis,
+                "valueXField": "date",
+                "valueYField": "avg_runtime",
+                "stroke": "green",
+                "fill": "green",
+                "stacked": true,
+                "tooltip": am5.Tooltip.new( root, {
+                    "labelText": "Average runtime: {valueY} sec.",
+                } ),
+            } ) );
 
-                        // pre-zoom
-                        "start": 0.75,
-                        "end": 1,
-                        "keepSelection": true,
-                    },
-                ],
-                "yAxes": [
-                    {
-                        "id": "value",
-                        "type": "ValueAxis",
-                        "title": { "text": "Seconds" },
-                        "min": 0,
-                        "numberFormatter": { "type": "NumberFormatter", "numberFormat": "#.##" },
-                    },
-                ],
-
-                "series": [
-                    {
-                        "id": "runtime",
-                        "type": "ColumnSeries",
-                        "name": "Runtime",
-                        "yAxis": "value",
-                        "dataFields": {
-                            "dateX": "date",
-                            "valueY": "avg_runtime",
-                        },
-                        "groupFields": { "valueY": "average" },
-                        "tooltipText": "Average runtime: {valueY.value} sec.",
-                        "stacked": true,
-                        "fill": "green",
-                        "stroke": "green",
-                        "columns": {
-                            "maxWidth": 30,
-                        },
-                    },
-                ],
+            series1.data.processor = am5.DataProcessor.new( root, {
+                "dateFields": ["date"],
+                "dateFormat": "i",
             } );
 
             this.refresh();
         },
 
-        // XXX
-        _exceptionsChartReady ( chart ) {
-            this.exceptionsChart = chart;
+        _createExceptionsChart ( cmp ) {
+            this._exceptionsChart = cmp;
 
-            chart.create( {
-                "titles": [{ "text": "Exceptions Percent for Last 30 Days", "fontSize": 12 }],
-                "type": "XYChart",
+            const root = cmp.root,
+                am5 = cmp.am5;
 
-                "dateFormatter": { "inputDateFormat": "yyyy-MM-dd HH:mm:ss" },
+            const chart = root.container.children.push( am5xy.XYChart.new( root, {
+                "layout": root.verticalLayout,
+                "panX": true,
+                "panY": true,
+                "pinchZoomX": true,
 
-                "cursor": { "type": "XYCursor", "behavior": "zoomX" },
+                // "wheelX": "panX",
+                // "wheelY": "zoomX",
+            } ) );
 
-                "scrollbarX": {
-                    "type": "XYChartScrollbar",
-                    "series": ["exceptions"],
+            chart.set( "cursor",
+                am5xy.XYCursor.new( root, {
+                    "behavior": "zoomX",
+                } ) );
+
+            // add scrollbar, https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+            chart.set( "scrollbarX",
+                am5.Scrollbar.new( root, {
+                    "orientation": "horizontal",
+                } ) );
+
+            chart.children.unshift( am5.Label.new( root, {
+                "text": "Exceptions Percent for Last 30 Days",
+                "fontSize": 12,
+                "x": am5.percent( 50 ),
+                "centerX": am5.percent( 50 ),
+            } ) );
+
+            const xAxis = chart.xAxes.push( am5xy.DateAxis.new( root, {
+                "maxDeviation": 0,
+                "baseInterval": {
+                    "timeUnit": "hour",
+                    "count": 1,
                 },
+                "renderer": am5xy.AxisRendererX.new( root, {} ),
+                "tooltipDateFormat": "I",
+                "tooltip": am5.Tooltip.new( root, {} ),
+            } ) );
 
-                "xAxes": [
-                    {
-                        "type": "DateAxis",
+            const yAxis = chart.yAxes.push( am5xy.ValueAxis.new( root, {
+                "renderer": am5xy.AxisRendererY.new( root, {} ),
+            } ) );
 
-                        // grouping
-                        "groupData": true,
-                        "groupCount": 24 * 30,
+            const series1 = chart.series.push( am5xy.ColumnSeries.new( root, {
+                "name": "Exceptions",
+                "xAxis": xAxis,
+                "yAxis": yAxis,
+                "valueXField": "date",
+                "valueYField": "errors_percent",
+                "stroke": "red",
+                "fill": "red",
+                "stacked": true,
+                "tooltip": am5.Tooltip.new( root, {
+                    "labelText": "Exceptions: {valueY}%",
+                } ),
+            } ) );
 
-                        // pre-zoom
-                        "start": 0.75,
-                        "end": 1,
-                        "keepSelection": true,
-                    },
-                ],
-                "yAxes": [
-                    {
-                        "id": "value",
-                        "type": "ValueAxis",
-                        "title": { "text": "Exceptions (%)" },
-                        "min": 0,
-                        "max": 100,
-                        "strictMinMax": true,
-                        "numberFormatter": { "type": "NumberFormatter", "numberFormat": "#.##" },
-                    },
-                ],
+            chart.series.push( am5xy.ColumnSeries.new( root, {
+                "name": "Declined",
+                "xAxis": xAxis,
+                "yAxis": yAxis,
+                "valueXField": "date",
+                "valueYField": "total_declined",
+                "fill": "red",
+                "stroke": "red",
+                "stacked": true,
+                "tooltip": am5.Tooltip.new( root, {
+                    "labelText": "Declined requests: {valueY}",
+                } ),
+            } ) );
 
-                "series": [
-                    {
-                        "id": "exceptions",
-                        "type": "ColumnSeries",
-                        "name": "Exceptions",
-                        "yAxis": "value",
-                        "dataFields": {
-                            "dateX": "date",
-                            "valueY": "errors_percent",
-                        },
-                        "groupFields": { "valueY": "average" },
-                        "tooltipText": "Exceptions: {valueY.value}%",
-                        "stacked": true,
-                        "fill": "red",
-                        "stroke": "red",
-                        "columns": {
-                            "maxWidth": 30,
-                        },
-                    },
-                ],
+            series1.data.processor = am5.DataProcessor.new( root, {
+                "dateFields": ["date"],
+                "dateFormat": "i",
             } );
 
             this.refresh();
@@ -269,7 +289,7 @@ export default {
         },
 
         async refresh () {
-            if ( !this.record || !this._loadChart || !this.runtimeChart || !this.exceptionsChart ) return;
+            if ( !this.record || !this._loadChart || !this._runtimeChart || !this._exceptionsChart ) return;
 
             this.ext.mask();
 
@@ -284,9 +304,8 @@ export default {
             }
 
             this._loadChart.setData( res.data );
-
-            this.runtimeChart.setData( res.data );
-            this.exceptionsChart.setData( res.data );
+            this._runtimeChart.setData( res.data );
+            this._exceptionsChart.setData( res.data );
         },
     },
 };
