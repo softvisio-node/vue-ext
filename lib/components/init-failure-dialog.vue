8adbda7a1b32bdbd6cc50151bb12e825119138ee
zdm <zdm@softvisio.net>
2022-06-17 17:39:20 +0300

    fix: dialogs focus

--- lib/components/init-failure-dialog.vue
+++ lib/components/init-failure-dialog.vue
@@ -1,9 +1,9 @@
 <template>
     <ext-dialog :title="i18nd(`vue-ext`, `Connection error`)" width="250" height="300" closable="false" shadow="true">
-        <ext-panel layout="center" :html='i18nd(`vue-ext`, `Unable to connect to the API server. Check, that you have internet connection and press "Try Again" button below.`)'/>
+        <ext-panel layout="center" :html='i18nd(`vue-ext`, `Unable to connect to the API server. Check, that you have internet connection and press "Try again" button below.`)'/>
 
         <ext-toolbar docked="bottom" layout='{"type":"hbox","pack":"center"}'>
-            <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Try Again`)" ui="action" @tap="close"/>
+            <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Try again`)" ui="action" @tap="close"/>
         </ext-toolbar>
     </ext-dialog>
 </template>
