f64b9dacee3d8a7f432dd92f75b9fb49b0a0ad0e
zdm <zdm@softvisio.net>
2022-06-19 03:16:24 +0300

    fix: webpack chunks

--- resources/webpack.vue.app.config.js
+++ resources/webpack.vue.app.config.js
@@ -15,47 +15,47 @@ config.resolve.alias = {
 
 // config.module.rules[1].exclude.push( /[\\/]resources[\\/]ext-[\d.]+[\\/]/, /[\\/]resources[\\/]ewc-[\d.]+[\\/]/ );
 
-config.optimization.splitChunks.cacheGroups["ext-charts"] = {
-    "name": "ext-charts",
-    test ( module ) {
-        if ( !module.resource ) return;
+// config.optimization.splitChunks.cacheGroups["ext-charts"] = {
+//     "name": "ext-charts",
+//     test ( module ) {
+//         if ( !module.resource ) return;
 
-        const resource = module.resource.replaceAll( "\\", "/" );
+//         const resource = module.resource.replaceAll( "\\", "/" );
 
-        if ( resource.includes( "@softvisio/ext/lib/ext-charts-" ) ) return true;
+//         if ( resource.includes( "@softvisio/ext/lib/ext-charts-" ) ) return true;
 
-        if ( resource.includes( "@softvisio/ext/resources/ext-" + process.env.EXT_VERSION + "/charts.js" ) ) return true;
-    },
-    "priority": -9,
-    "chunks": "all",
-};
+//         if ( resource.includes( "@softvisio/ext/resources/ext-" + process.env.EXT_VERSION + "/charts.js" ) ) return true;
+//     },
+//     "priority": -9,
+//     "chunks": "all",
+// };
 
-config.optimization.splitChunks.cacheGroups["ext"] = {
-    "name": "ext",
-    "test": /@softvisio[\\/]ext[\\/]/,
-    "priority": -9,
-    "chunks": "initial",
-};
+// config.optimization.splitChunks.cacheGroups["ext"] = {
+//     "name": "ext",
+//     "test": /@softvisio[\\/]ext[\\/]/,
+//     "priority": -9,
+//     "chunks": "initial",
+// };
 
-config.optimization.splitChunks.cacheGroups["froala-editor"] = {
-    "name": "froala-editor",
-    "test": /froala-editor[\\/]/,
-    "priority": -9,
-    "chunks": "all",
-};
+// config.optimization.splitChunks.cacheGroups["froala-editor"] = {
+//     "name": "froala-editor",
+//     "test": /froala-editor[\\/]/,
+//     "priority": -9,
+//     "chunks": "all",
+// };
 
-config.optimization.splitChunks.cacheGroups["amcharts4"] = {
-    "name": "amcharts4",
-    "test": /@amcharts[\\/]amcharts4[\\/]/,
-    "priority": -9,
-    "chunks": "all",
-};
+// config.optimization.splitChunks.cacheGroups["amcharts4"] = {
+//     "name": "amcharts4",
+//     "test": /@amcharts[\\/]amcharts4[\\/]/,
+//     "priority": -9,
+//     "chunks": "all",
+// };
 
-config.optimization.splitChunks.cacheGroups["pdfjs"] = {
-    "name": "pdfjs",
-    "test": /pdfjs-dist[\\/]/,
-    "priority": -9,
-    "chunks": "all",
-};
+// config.optimization.splitChunks.cacheGroups["pdfjs"] = {
+//     "name": "pdfjs",
+//     "test": /pdfjs-dist[\\/]/,
+//     "priority": -9,
+//     "chunks": "all",
+// };
 
 export default config;
