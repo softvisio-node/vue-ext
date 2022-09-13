9ef5e52e064eb13a7177bcfe1c4ba76de54f7d53
zdm <zdm@softvisio.net>
2022-09-13 12:41:44 +0300

    feat: user locale

--- lib/components/locale.button.vue
+++ lib/components/locale.button.vue
@@ -45,9 +45,10 @@ export default {
             for ( const [localeId, text] of Object.entries( locale.locales ) ) {
                 menu.push( {
                     "xtype": "menuradioitem",
-                    text,
+                    "id": localeId,
                     "value": localeId,
-                    "group": "value",
+                    text,
+                    "group": "locale",
                     "checked": localeId === locale.id,
                     "handler": () => this._setLocale( localeId ),
                 } );
@@ -58,8 +59,22 @@ export default {
             cmp.setText( locale.name );
         },
 
-        _setLocale ( localeId ) {
-            locale.setLocale( localeId );
+        async _setLocale ( localeId ) {
+            const menu = this.$refs.button.ext.getMenu();
+
+            menu.mask();
+
+            const res = await this.$store.session.setLocale( localeId );
+
+            menu.unmask();
+
+            if ( !res.ok ) {
+                this.$utils.toast( res );
+
+                menu.getItems().each( item => {
+                    item.setChecked( item.id === locale.id, true );
+                } );
+            }
         },
     },
 };
