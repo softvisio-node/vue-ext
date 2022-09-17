a4a636225907e86566fa53abaf336ccd7aea99c6
zdm <zdm@softvisio.net>
2022-09-17 17:55:25 +0300

    fix: username is email

--- lib/components/account/panel.vue
+++ lib/components/account/panel.vue
@@ -52,19 +52,7 @@
                 <ext-container :html="i18nd(`vue-ext`, `Interface theme`)"/>
             </ext-toolbar>
 
-            <ext-fieldset defaults='{"labelAlign":"left","labelWidth":200}'>
-                <ext-togglefield :label="i18nd(`vue-ext`, `Use OS theme`)" :value="systemDarkMode" @change="systemDarkMode = $event"/>
-
-                <DarkModeButton :disabled="systemDarkMode"/>
-
-                <ext-fieldcontainer :label="i18nd(`vue-ext`, `Base color`)" layout="fit">
-                    <ext-dataview inline="true" @childtap="themeBaseChanged" @ready="themesColorsViewReady"/>
-                </ext-fieldcontainer>
-
-                <ext-fieldcontainer :label="i18nd(`vue-ext`, `Accent color`)" layout="fit">
-                    <ext-componentdataview inline="true" @childtap="themeAccentChanged" @ready="themesColorsViewReady"/>
-                </ext-fieldcontainer>
-            </ext-fieldset>
+            <ThemePanel/>
         </ext-panel>
 
         <slot name="bottom"/>
@@ -75,22 +63,10 @@
 import UserSessionsPanel from "#lib/components/user-sessions/panel";
 import NotificationsPanel from "#lib/components/notifications/panel";
 import UserTokensPanel from "#lib/components/user-tokens/panel";
-import DarkModeButton from "#lib/components/dark-mode.button";
+import ThemePanel from "./theme.panel";
 
 export default {
-    "components": { UserSessionsPanel, NotificationsPanel, UserTokensPanel, DarkModeButton },
-
-    "computed": {
-        "systemDarkMode": {
-            get () {
-                return this.$store.theme.systemDarkMode;
-            },
-
-            set ( e ) {
-                this.$store.theme.setSystemDarkMode( e.detail.newValue );
-            },
-        },
-    },
+    "components": { UserSessionsPanel, NotificationsPanel, UserTokensPanel, ThemePanel },
 
     "methods": {
         _ready ( e ) {
@@ -157,44 +133,6 @@ export default {
             if ( !res.ok ) {
                 this.$utils.toast( res );
             }
-        },
-
-        themesColorsViewReady ( e ) {
-            const cmp = e.detail.cmp;
-
-            cmp.setItemTpl( `
-                        <div style="cursor:pointer;margin:5px 5px 5px 5px;border:1px dotted">
-                            <div style="width:20px;height:20px;background-color:{color}" data-qtip="{title}"></div>
-                        </div>
-                    ` );
-
-            const colors = Ext.theme.Material.getColors();
-
-            const store = new Ext.data.Store( {
-                "data": Object.keys( colors )
-                    .sort()
-                    .map( name => {
-                        return {
-                            "name": name,
-                            "title": name.replaceAll( /(-.)/g, match => match.replace( "-", " " ).toUpperCase() ).replace( /^./, match => match.toUpperCase() ),
-                            "color": colors[name][500],
-                        };
-                    } ),
-            } );
-
-            cmp.setStore( store );
-        },
-
-        themeBaseChanged ( e ) {
-            const record = e.detail.location.record;
-
-            this.$store.theme.setTheme( { "base": record.get( "name" ) } );
-        },
-
-        themeAccentChanged ( e ) {
-            const record = e.detail.location.record;
-
-            this.$store.theme.setTheme( { "accent": record.get( "name" ) } );
         },
 
         async changePassword () {
