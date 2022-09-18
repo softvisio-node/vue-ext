aff372e239fe74e59f3536171a383627de7c6404
zdm <zdm@softvisio.net>
2022-09-19 01:18:36 +0300

    fix: username is email

--- lib/components/notifications/panel.vue
+++ lib/components/notifications/panel.vue
@@ -1,24 +1,5 @@
 <template>
     <ext-panel defaults='{"padding":"0 0 30 0"}' layout="vbox" @ready="_ready">
-        <!-- <ext-container layout="hbox"> -->
-        <!--     <ext-textfield :label="i18nd( `vue-ext`,`Yout Email Address`)" labelAlign="left" labelWidth="150" :value="email"/> -->
-        <!--     <ext-button :text="i18nd( `vue-ext`,`Update`)"/> -->
-        <!-- </ext-container> -->
-
-        <!-- telegram settings -->
-        <ext-container :hidden="telegramHidden">
-            <ext-toolbar docked="top">
-                <ext-container html="Telegram"/>
-                <ext-spacer/>
-                <ext-button iconAlign="right" iconCls="fa-solid fa-external-link-alt" :text="i18nd(`vue-ext`, `Open bot`)" @tap="_openTelegramBot"/>
-            </ext-toolbar>
-
-            <ext-container layout='{"pack":"start","type":"hbox"}'>
-                <ext-textfield ref="telegramUsernameField" flex="1" :label="i18nd(`vue-ext`, `Your Telegram username`)" labelAlign="left" labelWidth="150" :value="telegramUsername"/>
-                <ext-button :text="i18nd(`vue-ext`, `Save username`)" @tap="_updateTelegramUsername"/>
-            </ext-container>
-        </ext-container>
-
         <!-- push notifications -->
         <ext-panel :hidden="pusHidden">
             <ext-toolbar docked="top">
@@ -32,19 +13,15 @@
         </ext-panel>
 
         <!-- notification types -->
-        <ext-container :hidden="telegramHidden">
-            <div>{{ i18nd( `vue-ext`,msgid`In order to receive telegram notifications you need to set your telegram username. Then open chat with the <b>@${telegramBotUsername}</b> and press <b>"Start"</b>.`) }}</div>
-        </ext-container>
-
         <ext-grid columnMenu="false" columnResize="false" flex="1" itemConfig='{"viewModel":true}' sortable="false" @ready="_gridReady">
             <ext-toolbar docked="top">
                 <ext-container :html="i18nd(`vue-ext`, `Notification types`)"/>
             </ext-toolbar>
             <ext-column cell='{"encodeHtml":false}' dataIndex="title" flex="1"/>
-            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Internal${`</b><br/>`}notifications`) + '</div>'" width="100" @ready="_internalColReady"/>
-            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Email${`</b><br/>`}notifications`) + '</div>'" width="100" @ready="_emailColReady"/>
-            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Telegram${`</b><br/>`}notifications`) + '</div>'" width="100" @ready="_telegramColReady"/>
-            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Push${`</b><br/>`}notifications`) + '</div>'" width="100" @ready="_pushColReady"/>
+            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Internal${`</b><br/>`}notifications`) + '</div>'" width="110" @ready="_internalColReady"/>
+            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Email${`</b><br/>`}notifications`) + '</div>'" width="110" @ready="_emailColReady"/>
+            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Telegram${`</b><br/>`}notifications`) + '</div>'" width="110" @ready="_telegramColReady"/>
+            <ext-column align="center" :text="`<div style=&quot;text-align:center&quot;><b>` + i18nd(`vue-ext`, msgid`Push${`</b><br/>`}notifications`) + '</div>'" width="110" @ready="_pushColReady"/>
         </ext-grid>
     </ext-panel>
 </template>
@@ -56,26 +33,6 @@ export default {
     "components": { PushNotificationsButton },
 
     "computed": {
-        email () {
-            return this.$store.notifications.email;
-        },
-
-        telegramUsername () {
-            return this.$store.notifications.telegramUsername;
-        },
-
-        telegramHidden () {
-            return !this.$store.notifications.telegramBotUsername;
-        },
-
-        telegramBotUsername () {
-            return this.$store.notifications.telegramBotUsername;
-        },
-
-        telegramBotUrl () {
-            return "https://t.me/" + this.$store.notifications.telegramBotUsername;
-        },
-
         pusHidden () {
             return !this.$store.session.pushNotificationsSupported;
         },
@@ -130,7 +87,6 @@ export default {
             } );
         },
 
-        // XXX
         async reload () {
             await this.$store.notifications.reloadSettings();
         },
@@ -157,31 +113,6 @@ export default {
             }
 
             return;
-        },
-
-        _openTelegramBot () {
-            window.open( this.telegramBotUrl, "_blank" ).focus();
-        },
-
-        async _updateTelegramUsername ( e ) {
-            const button = e.detail.sender,
-                field = this.$refs.telegramUsernameField.ext,
-                value = field.getValue();
-
-            button.disable();
-
-            const res = await this.$api.call( "account/set-telegram-username", value );
-
-            button.enable();
-
-            if ( !res.ok ) {
-                this.$utils.toast( res );
-            }
-            else {
-                this.$store.notifications.telegramUsername = value;
-
-                this.$utils.toast( this.i18nd( `vue-ext`, `Telegram username updated` ) );
-            }
         },
     },
 };
