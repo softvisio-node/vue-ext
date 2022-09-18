75465156cb19708cdd854348f9d37ee484de6fa7
zdm <zdm@softvisio.net>
2022-09-18 20:00:12 +0300

    fix: username is email

--- lib/components/change-email.dialog.vue
+++ lib/components/change-email.dialog.vue
@@ -1,12 +1,15 @@
 <template>
-    <ext-dialog closeAction="hide" height="300" :title="title" width="300" @ready="_ready">
+    <ext-dialog closeAction="destroy" height="300" :title="title" width="300" @ready="_ready">
         <ext-fieldpanel ref="form" @ready="formReady">
-            <ext-emailfield :label="i18nd(`vue-ext`, `New email address`)" name="email" :placeholder="i18nd(`vue-ext`, `Ender new email address`)" required="true" validators="email"/>
+            <ext-emailfield :label="i18nd(`vue-ext`, `New email address`)" name="email" :placeholder="i18nd(`vue-ext`, `Enter new email address`)" required="true" validators="email"/>
+
+            <ext-textfield :hidden="true" :label="i18nd(`vue-ext`, `Token`)" name="token" :placeholder="i18nd(`vue-ext`, `Enter token`)" required="true"/>
         </ext-fieldpanel>
 
         <ext-toolbar docked="bottom" layout='{"pack":"end","type":"hbox"}'>
-\ <ext-button :text="i18nd(`vue-ext`, `Change email`)" ui="action" @tap="submit"/>
-</ext-toolbar>
+            <ext-button ref="sendTokenButton" :text="i18nd(`vue-ext`, `Send token`)" ui="action" @tap="_sendToken"/>
+            <ext-button ref="setEmailButton" :hidden="true" :text="i18nd(`vue-ext`, `Change email`)" ui="action" @tap="_setEmail"/>
+        </ext-toolbar>
     </ext-dialog>
 </template>
 
@@ -23,26 +26,33 @@ export default {
     "methods": {
         _ready ( e ) {
             this.ext = e.detail.cmp;
-
-            this.ext.on( "hide", () => {
-                this.$refs.form.ext.reset();
-            } );
         },
 
         formReady ( e ) {
             var cmp = e.detail.cmp;
 
-            cmp.setKeyMap( { "ENTER": { "handler": "submit", "scope": this } } );
+            cmp.setKeyMap( {
+                "ENTER": {
+                    "handler": () => {
+                        if ( this.$refs.sendTokenButton.ext.isVisible() ) {
+                            this._sendToken();
+                        }
+                        else if ( this.$refs.setEmailButton.ext.isVisible() ) {
+                            this._setEmail();
+                        }
+                    },
+                },
+            } );
         },
 
         close () {
-            this.ext.hide();
+            this.ext.destroy();
         },
 
-        async submit () {
+        async _sendToken () {
             const form = this.$refs.form.ext;
 
-            if ( !form.validate() ) return;
+            if ( !form.getFields( "email" ).validate() ) return;
 
             const values = form.getValues();
 
@@ -53,7 +63,33 @@ export default {
             this.ext.unmask();
 
             if ( res.ok ) {
-                this.$utils.toast( this.i18nd( `vue-ext`, "Change email instructions were sent to the new email address" ) );
+                this.$utils.toast( this.i18nd( `vue-ext`, "Change email token was sent to the new email address" ) );
+
+                form.getFields( "email" ).setReadOnly( true );
+                form.getFields( "token" ).show();
+                this.$refs.sendTokenButton.ext.hide();
+                this.$refs.setEmailButton.ext.show();
+            }
+            else {
+                this.$utils.toast( res );
+            }
+        },
+
+        async _setEmail () {
+            const form = this.$refs.form.ext;
+
+            if ( !form.getFields( "token" ).validate() ) return;
+
+            const values = form.getValues();
+
+            this.ext.mask( loadMask );
+
+            const res = await this.$api.call( "account/set-email-by-token", values.token );
+
+            this.ext.unmask();
+
+            if ( res.ok ) {
+                this.$utils.toast( this.i18nd( `vue-ext`, "Email address changed" ) );
 
                 this.close();
             }
