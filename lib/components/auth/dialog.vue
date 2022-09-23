9b476366eefda16fcf2227f56ada8580f85ee36b
zdm <zdm@softvisio.net>
2022-09-23 09:39:30 +0300

    fix: password

--- lib/components/auth/dialog.vue
+++ lib/components/auth/dialog.vue
@@ -13,7 +13,7 @@ export default {
     "props": {
         "errorTarget": {
             "type": String,
-            "default": "qtip",
+            "default": "under",
         },
         "resetPasswordEnabled": {
             "type": Boolean,
--- lib/components/auth/panel.vue
+++ lib/components/auth/panel.vue
@@ -24,7 +24,7 @@ export default {
     "props": {
         "errorTarget": {
             "type": String,
-            "default": "qtip",
+            "default": "under",
         },
         "resetPasswordEnabled": {
             "type": Boolean,
