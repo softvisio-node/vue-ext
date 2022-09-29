89f49db454418f73205c855df168693ae5767448
zdm <zdm@softvisio.net>
2022-09-29 09:45:15 +0300

    fix: card error panel

---
+++ lib/components/cards-error.panel.vue
@@ -0,0 +1,118 @@
+<template>
+    <ext-panel ref="cards" layout="card" viewModel="true" @ready="_ready">
+        <!-- no data card -->
+        <ext-panel ref="noDataCard" :html="i18nd(`vue-ext`, `No data match search criteria`)" layout="center"/>
+
+        <!-- error card -->
+        <ext-container ref="errorCard" layout='{"align":"center","pack":"center","type":"vbox"}' style="text-align: center">
+            <ext-container :html="i18nd(`vue-ext`, `Unable to load data`)"/>
+            <ext-container ref="errorMessage"/>
+            <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Refresh`)" ui="action" @tap="reload"/>
+        </ext-container>
+
+        <ext-panel ref="dataCard" layout="fit">
+            <slot/>
+        </ext-panel>
+    </ext-panel>
+</template>
+
+<script>
+export default {
+    "props": {
+        "store": {
+            "type": Object,
+            "default": null,
+        },
+        "showErrorMessage": {
+            "type": Boolean,
+            "default": true,
+        },
+    },
+
+    "emits": ["reload"],
+
+    mounted () {
+        if ( this.store ) this.watchStore( this.store );
+    },
+
+    unmounted () {
+        this.unwatchStore( this.store );
+    },
+
+    "methods": {
+        reload () {
+            this.$emit( "reload" );
+        },
+
+        watchStore ( store ) {
+            this._store = store;
+            this._storeLoadCallback = this._onStoreLoad.bind( this );
+            this._storeDataChangedCallback = this._onStoreDataChanged.bind( this );
+
+            this._store.on( "load", this._storeLoadCallback );
+            this._store.on( "datachanged", this._storeDataChangedCallback );
+        },
+
+        unwatchStore () {
+            if ( !this._store ) return;
+
+            this._store.off( "load", this._storeLoadCallback );
+            this._store.off( "datachanged", this._storeDataChangedCallback );
+
+            this._store = null;
+            this._storeLoadCallback = null;
+            this._storeDataChangedCallback = null;
+        },
+
+        setResult ( res ) {
+            if ( !res.ok ) {
+                this.showErrorCard( res );
+            }
+            else {
+                this.showDataCard();
+            }
+        },
+
+        showErrorCard ( message ) {
+            if ( message && this.showErrorMessage ) {
+                this.$refs.errorMessage.setHtml( message + "" );
+            }
+            else {
+                this.$refs.errorMessage.setHtml( "" );
+            }
+
+            this.$refs.cards.ext.setActiveItem( this.$refs.errorCard.ext );
+        },
+
+        showNoDataCard () {
+            this.$refs.cards.ext.setActiveItem( this.$refs.noDataCard.ext );
+        },
+
+        showDataCard () {
+            this.$refs.cards.ext.setActiveItem( this.$refs.dataCard.ext );
+        },
+
+        // XXX
+        _onStoreLoad ( store, records, success ) {
+            if ( !success ) {
+                this.showErrorCard();
+            }
+            else if ( !this.store.count() ) {
+                this.showNoDataCard();
+            }
+            else {
+                this.showDataCard();
+            }
+        },
+
+        _onStoreDataChanged ( store ) {
+            if ( !store.getCount() ) {
+                this.showNoDataCard();
+            }
+            else {
+                this.showDataCard();
+            }
+        },
+    },
+};
+</script>
