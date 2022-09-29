1c0f6645955309a47ead79ee050db69a32804637
zdm <zdm@softvisio.net>
2022-09-29 17:48:37 +0300

    fix: card error panel

--- lib/components/user-sessions/panel.vue
+++ lib/components/user-sessions/panel.vue
@@ -1,45 +1,41 @@
 <template>
-    <ext-panel ref="cards" layout="card">
-        <ext-toolbar docked="top">
-            <ext-container :html="title"/>
-            <ext-spacer/>
-            <ext-button iconCls="fa-solid fa-sign-out-alt" :text="i18nd(`vue-ext`, `Sign out of all sessions`)" @tap="_signOutAllSessions"/>
-            <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Refresh`)" @tap="reload"/>
-        </ext-toolbar>
+    <CardsErrorPanel ref="cards" :store="store" @reload="reload">
+        <template #items>
+            <ext-toolbar docked="top">
+                <ext-container :html="title"/>
+                <ext-spacer/>
+                <ext-button iconCls="fa-solid fa-sign-out-alt" :text="i18nd(`vue-ext`, `Sign out of all sessions`)" @tap="_signOutAllSessions"/>
+                <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Refresh`)" @tap="reload"/>
+            </ext-toolbar>
+        </template>
 
-        <!-- no-data card -->
-        <ext-container ref="noDataCard" :html="i18nd(`vue-ext`, `No data match search criteria`)" layout="center" style="text-align: center"/>
+        <template #data>
+            <ext-grid ref="dataCard" layout="fit" multicolumnSort="true" plugins='{"gridviewoptions":true}' @ready="_gridReady">
+                <ext-column cell='{"encodeHtml":false}' dataIndex="device_text" flex="1" sorter='{"property":"device_vendor"}' :text="i18nd(`vue-ext`, `Device`)"/>
 
-        <!-- error card -->
-        <ext-container ref="errorCard" layout='{"align":"center","pack":"center","type":"vbox"}' style="text-align: center">
-            <ext-container :html="i18nd(`vue-ext`, `Unable to load data`)"/>
-            <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Refresh`)" ui="action" @tap="reload"/>
-        </ext-container>
+                <ext-column cell='{"encodeHtml":false}' dataIndex="os_text" flex="1" sorter='{"property":"os_name"}' :text="i18nd(`vue-ext`, `Platform`)"/>
 
-        <!-- data card -->
-        <ext-grid ref="dataCard" layout="fit" multicolumnSort="true" plugins='{"gridviewoptions":true}' @ready="_gridReady">
-            <ext-column cell='{"encodeHtml":false}' dataIndex="device_text" flex="1" sorter='{"property":"device_vendor"}' :text="i18nd(`vue-ext`, `Device`)"/>
+                <ext-column cell='{"encodeHtml":false}' dataIndex="browser_text" flex="1" sorter='{"property":"browser_name"}' :text="i18nd(`vue-ext`, `Browser`)"/>
 
-            <ext-column cell='{"encodeHtml":false}' dataIndex="os_text" flex="1" sorter='{"property":"os_name"}' :text="i18nd(`vue-ext`, `Platform`)"/>
+                <ext-column dataIndex="remote_address" :text="i18nd(`vue-ext`, `IP address`)" width="130"/>
 
-            <ext-column cell='{"encodeHtml":false}' dataIndex="browser_text" flex="1" sorter='{"property":"browser_name"}' :text="i18nd(`vue-ext`, `Browser`)"/>
+                <ext-column cell='{"encodeHtml":false}' dataIndex="last_activity_text" sorter='{"property":"last_activity"}' :text="i18nd(`vue-ext`, `Last activity`)" width="150"/>
 
-            <ext-column dataIndex="remote_address" :text="i18nd(`vue-ext`, `IP address`)" width="130"/>
+                <ext-column dataIndex="created" formatter="date()" :text="i18nd(`vue-ext`, `Creation date`)" width="150"/>
 
-            <ext-column cell='{"encodeHtml":false}' dataIndex="last_activity_text" sorter='{"property":"last_activity"}' :text="i18nd(`vue-ext`, `Last activity`)" width="150"/>
-
-            <ext-column dataIndex="created" formatter="date()" :text="i18nd(`vue-ext`, `Creation date`)" width="150"/>
-
-            <ext-column width="50" @ready="_actionColReady"/>
-        </ext-grid>
-    </ext-panel>
+                <ext-column width="50" @ready="_actionColReady"/>
+            </ext-grid>
+        </template>
+    </CardsErrorPanel>
 </template>
 
 <script>
 import SessionModel from "./models/session";
-import loadMask from "#vue/load-mask";
+import CardsErrorPanel from "#lib/components/cards-error.panel";
 
 export default {
+    "components": { CardsErrorPanel },
+
     "computed": {
         title () {
             return this.i18nd( `vue-ext`, `Your sessions` );
@@ -54,8 +50,6 @@ export default {
             "remoteSort": false,
             "remoteFilter": false,
         } );
-
-        this.store.on( "datachanged", this._onStoreChanged.bind( this ) );
     },
 
     "methods": {
@@ -129,28 +123,19 @@ export default {
         },
 
         async reload () {
-            this.$refs.cards.ext.mask( loadMask );
+            this.$refs.cards.mask();
 
             const res = await this._loadSessions();
 
-            this.$refs.cards.ext.unmask();
+            this.$refs.cards.unmask();
 
             if ( !res.ok ) {
-                this.$utils.toast( res );
+                this.$refs.cards.setResult( res );
 
-                this.$refs.cards.ext.setActiveItem( this.$refs.errorCard.ext );
+                this.$utils.toast( res );
             }
             else {
                 this.store.loadRawData( res.data );
-            }
-        },
-
-        _onStoreChanged ( store, records, success ) {
-            if ( !this.store.count() ) {
-                this.$refs.cards.ext.setActiveItem( this.$refs.noDataCard.ext );
-            }
-            else {
-                this.$refs.cards.ext.setActiveItem( this.$refs.dataCard.ext );
             }
         },
 
