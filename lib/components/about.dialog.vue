e6aa27347103fe6783837c37aa5721d60e33a7f9
zdm <zdm@softvisio.net>
2022-12-16 00:29:27 +0200

    feat: acl refactored

--- lib/components/about.dialog.vue
+++ lib/components/about.dialog.vue
@@ -1,16 +1,12 @@
 <template>
-    <ext-dialog closeAction="hide" height="400" layout="vbox" :title="i18nd(`vue-ext`, `About the project`)" width="350" @ready="_ready">
-        <ext-container :html="header" style="text-align: center"/>
+    <ext-dialog closeAction="hide" height="400" layout="vbox" :title="i18nd(`vue-ext`, `About the project`)" width="350">
+        <ext-fieldpanel defaults='{"labelAlign":"left"}'>
+            <ext-displayfield :label="i18nd(`vue-ext`, `UI version`)" :value="'v' + frontend.currentVersion"/>
+            <ext-displayfield :label="i18nd(`vue-ext`, `UI mode`)" :value="frontend.mode"/>
 
-        <ext-fieldpanel ref="form" @ready="formReady">
-            <ext-passwordfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `New password`)" name="password" :placeholder="i18nd(`vue-ext`, `Enter new password`)" required="true" revealable="true"/>
-
-            <ext-passwordfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `Confirm new password`)" name="confirmedPassword" :placeholder="i18nd(`vue-ext`, `Confirm new password`)" required="true" revealable="true"/>
+            <ext-displayfield :label="i18nd(`vue-ext`, `Backend version`)" :value="'v' + backend.currentVersion"/>
+            <ext-displayfield :label="i18nd(`vue-ext`, `Backend mode`)" :value="backend.mode"/>
         </ext-fieldpanel>
-
-        <ext-container layout="center">
-            <ext-button :text="i18nd(`vue-ext`, `Generate random password`)" @tap="_generatePassword"/>
-        </ext-container>
 
         <ext-toolbar docked="bottom" layout='{"pack":"end","type":"hbox"}'>
             <ext-button iconCls="fa-regular fa-copy" :text="i18nd(`vue-ext`, `Copy to clipboard`)" ui="action" @tap="_copy"/>
@@ -20,40 +16,17 @@
 
 <script>
 export default {
-    "props": {
-        "errorTarget": {
-            "type": String,
-            "default": "under",
+    "computed": {
+        backend () {
+            return this.$app.backendGitId || {};
+        },
+
+        frontend () {
+            return this.$app.frontendGitId || {};
         },
     },
 
     "methods": {
-        _ready ( e ) {
-            this.ext = e.detail.cmp;
-
-            this.$refs.form.ext.getFields( "password" ).setValidators( {
-                "type": "password-strength",
-                "strength": this.$app.settings.passwordsStrength,
-            } );
-
-            this.ext.on( "hide", () => {
-                this.$refs.form.ext.reset();
-
-                this.$refs.form.ext.getFields( "password" ).setRevealed( false );
-                this.$refs.form.ext.getFields( "confirmedPassword" ).setRevealed( false );
-            } );
-        },
-
-        formReady ( e ) {
-            var cmp = e.detail.cmp;
-
-            cmp.setKeyMap( { "ENTER": { "handler": "submit", "scope": this } } );
-        },
-
-        close () {
-            this.ext.hide();
-        },
-
         _copy () {
             this.$utils.copyToClipboard( JSON.stringify(
                 {
