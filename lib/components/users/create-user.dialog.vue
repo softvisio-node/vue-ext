8ab998ae9b011ea928f5b7eb8e218b281523669b
zdm <zdm@softvisio.net>
2023-01-08 00:40:18 +0200

    fix: create user roles

--- lib/components/users/create-user.dialog.vue
+++ lib/components/users/create-user.dialog.vue
@@ -1,11 +1,11 @@
 <template>
-    <ext-dialog closeAction="hide" height="450" layout="vbox" scrollable="true" :title="i18nd(`vue-ext`, `User creation`)" width="350" @ready="_ready">
-        <ext-fieldpanel ref="form" @ready="formReady">
-            <ext-emailfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `Email`)" name="email" :placeholder="i18nd(`vue-ext`, `Enter email`)" required="true" validators="email"/>
+    <ext-dialog closeAction="destroy" height="90%" layout="vbox" scrollable="true" :title="i18nd(`vue-ext`, `User creation`)" width="600" @ready="_onReady">
+        <ext-fieldpanel ref="form" @ready="_onFormReady">
+            <ext-emailfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `Email`)" labelAlign="left" labelWidth="150" name="email" :placeholder="i18nd(`vue-ext`, `Enter email`)" required="true" validators="email"/>
 
-            <ext-passwordfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `Password`)" name="password" :placeholder="i18nd(`vue-ext`, `Enter password`)" required="true" revealable="true"/>
+            <ext-passwordfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `Password`)" labelAlign="left" labelWidth="150" name="password" :placeholder="i18nd(`vue-ext`, `Enter password`)" required="true" revealable="true"/>
 
-            <ext-passwordfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `Confirm password`)" name="confirmedPassword" :placeholder="i18nd(`vue-ext`, `Confirm password`)" required="true" revealable="true"/>
+            <ext-passwordfield :errorTarget="errorTarget" :label="i18nd(`vue-ext`, `Confirm password`)" labelAlign="left" labelWidth="150" name="confirmedPassword" :placeholder="i18nd(`vue-ext`, `Confirm password`)" required="true" revealable="true"/>
 
             <ext-container layout="center">
                 <ext-button :text="i18nd(`vue-ext`, `Generate random password`)" @tap="_generatePassword"/>
@@ -14,17 +14,23 @@
             <ext-togglefield :label="i18nd(`vue-ext`, `Access enabled`)" labelAlign="left" labelWidth="150" name="enabled" value="true"/>
         </ext-fieldpanel>
 
+        <RolesPanel ref="rolesPanel" :aclId="aclId" flex="1"/>
+
         <ext-toolbar docked="bottom">
             <ext-spacer/>
-            <ext-button :text="i18nd(`vue-ext`, `Create user`)" ui="action" @tap="submit"/>
+            <ext-button :text="i18nd(`vue-ext`, `Create user`)" ui="action" @tap="_submit"/>
         </ext-toolbar>
     </ext-dialog>
 </template>
 
 <script>
+import constants from "#core/app/constants";
 import passwords from "#core/utils/passwords";
+import RolesPanel from "#lib/components/acl/roles.panel";
 
 export default {
+    "components": { RolesPanel },
+
     "props": {
         "errorTarget": {
             "type": String,
@@ -34,34 +40,47 @@ export default {
 
     "emits": ["created"],
 
+    data () {
+        return {
+            "aclId": constants.defaultAclId,
+        };
+    },
+
     "methods": {
-        async _ready ( e ) {
+
+        // public
+        close () {
+            this.ext.destroy();
+        },
+
+        // protected
+        async _onReady ( e ) {
             this.ext = e.detail.cmp;
 
             this.$refs.form.ext.getFields( "password" ).setValidators( {
                 "type": "password-strength",
                 "strength": this.$app.settings.passwordsStrength,
             } );
-
-            this.ext.on( "hide", () => {
-                this.$refs.form.ext.reset();
-
-                this.$refs.form.ext.getFields( "password" ).setRevealed( false );
-                this.$refs.form.ext.getFields( "confirmedPassword" ).setRevealed( false );
-            } );
         },
 
-        formReady ( e ) {
+        _onFormReady ( e ) {
             var cmp = e.detail.cmp;
 
             cmp.setKeyMap( { "ENTER": { "handler": "submit", "scope": this } } );
         },
 
-        close () {
-            this.ext.hide();
+        _generatePassword () {
+            const password = passwords.generatePassword().password;
+
+            this.$utils.copyToClipboard( password );
+
+            this.$utils.toast( this.i18nd( `vue-ext`, `Password copied to the clipboard` ) );
+
+            this.$refs.form.ext.getFields( "password" ).setValue( password );
+            this.$refs.form.ext.getFields( "confirmedPassword" ).setValue( password );
         },
 
-        async submit () {
+        async _submit () {
             const form = this.$refs.form.ext;
 
             if ( !form.validate() ) return;
@@ -76,7 +95,9 @@ export default {
 
             delete values.confirmedPassword;
 
-            var res = await this.$api.call( "admin/users/create", values );
+            values.roles = this.$refs.rolesPanel.getEnabledRoles();
+
+            const res = await this.$api.call( "admin/users/create", values );
 
             if ( res.ok ) {
                 this.$utils.toast( this.i18nd( `vue-ext`, "User created" ) );
@@ -88,17 +109,6 @@ export default {
             else {
                 this.$utils.toast( res );
             }
-        },
-
-        _generatePassword () {
-            const password = passwords.generatePassword().password;
-
-            this.$utils.copyToClipboard( password );
-
-            this.$utils.toast( this.i18nd( `vue-ext`, `Password copied to the clipboard` ) );
-
-            this.$refs.form.ext.getFields( "password" ).setValue( password );
-            this.$refs.form.ext.getFields( "confirmedPassword" ).setValue( password );
         },
     },
 };
