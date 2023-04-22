9748593027d5586dd57dad8f880aebebf35de5d4
zdm <zdm@softvisio.net>
2023-04-22 20:02:49 +0300

    feat: administration component

---
+++ src/components/administration/dialog.vue
@@ -0,0 +1,32 @@
+<template>
+    <ext-dialog :height="height" layout="fit" scrollable="true" :title="i18nd(`vue-ext`, `Administration`)" :width="width">
+        <Panel>
+            <template #top>
+                <slot name="top"/>
+            </template>
+
+            <template #bottom>
+                <slot name="bottom"/>
+            </template>
+        </Panel>
+    </ext-dialog>
+</template>
+
+<script>
+import Panel from "./panel";
+
+export default {
+    "components": { Panel },
+
+    "props": {
+        "width": {
+            "type": String,
+            "default": "95%",
+        },
+        "height": {
+            "type": String,
+            "default": "95%",
+        },
+    },
+};
+</script>
---
+++ src/components/administration/panel.vue
@@ -0,0 +1,39 @@
+<template>
+    <ext-tabpanel defaults='{"padding":"0 10 0 10"}' layout='{"animation":{"direction":"vertical","type":"slide"}}' tabBar='{"defaults":{"flex":null,"height":40,"padding":"0 10 0 0","textAlign":"right","width":200},"layout":{"align":"start","pack":"start","type":"vbox"}}' tabBarPosition="left" tabRotation="none" @ready="_ready">
+        <slot name="top"/>
+
+        <!-- users -->
+        <ext-panel iconAlign="left" iconCls="fa-solid fa-users" layout="fit" :title="i18nd(`vue-ext`, `Users`)">
+            <UsersPanel/>
+        </ext-panel>
+
+        <!-- notifications -->
+        <ext-panel layout="fit" :title="i18nd(`vue-ext`, `Notifications`)">
+            <NotificationsPanel/>
+        </ext-panel>
+
+        <!-- tokens -->
+        <ext-panel iconAlign="left" iconCls="fa-solid fa-key" layout="fit" :title="i18nd(`vue-ext`, `Access tokens`)">
+            <UserTokensPanel/>
+        </ext-panel>
+
+        <slot name="bottom"/>
+    </ext-tabpanel>
+</template>
+
+<script>
+import UsersPanel from "#src/components/users/panel";
+import NotificationsPanel from "#src/components/notifications/panel";
+import UserTokensPanel from "#src/components/user-tokens/panel";
+
+export default {
+    "components": { UsersPanel, NotificationsPanel, UserTokensPanel },
+
+    "methods": {
+        _ready ( e ) {
+
+            // const cmp = e.detail.cmp;
+        },
+    },
+};
+</script>
