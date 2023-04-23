48cee32de96bd940b457a55a8ce53ddd8afe46ad
zdm <zdm@softvisio.net>
2023-04-23 22:11:39 +0300

    feat: administration component

--- src/components/administration/panel.vue
+++ src/components/administration/panel.vue
@@ -1,12 +1,12 @@
 <template>
     <ext-tabpanel defaults='{"padding":"0 10 0 10"}' layout='{"animation":{"direction":"vertical","type":"slide"}}' tabBar='{"defaults":{"flex":null,"height":40,"padding":"0 10 0 0","textAlign":"right","width":200},"layout":{"align":"start","pack":"start","type":"vbox"}}' tabBarPosition="left" tabRotation="none" @ready="_ready">
         <!-- users -->
-        <ext-panel iconAlign="left" iconCls="fa-solid fa-users" layout="fit" :title="i18nd(`vue-ext`, `Users`)">
+        <ext-panel layout="fit" :title="i18nd(`vue-ext`, `Users`)">
             <UsersPanel/>
         </ext-panel>
 
         <!-- api call log -->
-        <ext-panel :hidden="!apiCallLogEnabled" iconAlign="left" iconCls="fa-solid fa-heart-pulse" layout="fit" :title="i18nd(`vue-ext`, `API call log`)">
+        <ext-panel :hidden="!apiCallLogEnabled" layout="fit" :title="i18nd(`vue-ext`, `API call log`)">
             <ApiCallLogPanel/>
         </ext-panel>
 
