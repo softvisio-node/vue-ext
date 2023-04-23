89e9616f0fdf7853cac54feabe91927d0f31bc40
zdm <zdm@softvisio.net>
2023-04-23 18:59:15 +0300

    feat: administration component

--- src/components/api-call-log/log.dialog.vue
+++ src/components/api-call-log/log.dialog.vue
@@ -1,28 +1,37 @@
 <template>
     <ext-dialog height="95%" layout="fit" scrollable="true" :title="title" viewModel="true" width="95%">
-        <ext-toolbar docked="top">
-            <ext-spacer/>
-            <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Refresh`)" @tap="reload"/>
-        </ext-toolbar>
+        <CardsPanel ref="cardsPanel" :store="store" @reload="reload" @render="reload">
+            <template #items>
+                <ext-toolbar docked="top">
+                    <ext-spacer/>
+                    <ext-button iconCls="fa-solid fa-redo" :text="i18nd(`vue-ext`, `Refresh`)" @tap="reload"/>
+                </ext-toolbar>
+            </template>
 
-        <ext-grid ref="grid" layout="fit" multicolumnSort="true" @ready="gridReady">
-            <ext-column dataIndex="started" formatter='date("dateStyle:short,timeStyle:short")' :text="i18nd(`vue-ext`, `Started`)" width="150"/>
-            <ext-column dataIndex="finished" formatter='date("dateStyle:short,timeStyle:short")' :text="i18nd(`vue-ext`, `Finished`)" width="150"/>
-            <ext-column align="right" dataIndex="runtime_text" sortable="false" :text="i18nd(`vue-ext`, `Runtime (sec.)`)" width="150"/>
+            <template #data>
+                <ext-grid ref="grid" layout="fit" multicolumnSort="true" @ready="gridReady">
+                    <ext-column dataIndex="started" formatter='date("dateStyle:short,timeStyle:short")' :text="i18nd(`vue-ext`, `Started`)" width="150"/>
+                    <ext-column dataIndex="finished" formatter='date("dateStyle:short,timeStyle:short")' :text="i18nd(`vue-ext`, `Finished`)" width="150"/>
+                    <ext-column align="right" dataIndex="runtime_text" sortable="false" :text="i18nd(`vue-ext`, `Runtime (sec.)`)" width="150"/>
 
-            <ext-column align="center" cell='{"encodeHtml":false}' dataIndex="is_exception_text" sorter='{"property":"is_exception"}' :text="i18nd(`vue-ext`, `Exception`)" width="100"/>
+                    <ext-column align="center" cell='{"encodeHtml":false}' dataIndex="is_exception_text" sorter='{"property":"is_exception"}' :text="i18nd(`vue-ext`, `Exception`)" width="100"/>
 
-            <ext-column align="center" cell='{"encodeHtml":false}' dataIndex="status_label" sortable="false" :text="i18nd(`vue-ext`, `Status`)" width="100"/>
+                    <ext-column align="center" cell='{"encodeHtml":false}' dataIndex="status_label" sortable="false" :text="i18nd(`vue-ext`, `Status`)" width="100"/>
 
-            <ext-column dataIndex="status_text" flex="1" sortable="false" :text="i18nd(`vue-ext`, `Status text`)"/>
-        </ext-grid>
+                    <ext-column dataIndex="status_text" flex="1" sortable="false" :text="i18nd(`vue-ext`, `Status text`)"/>
+                </ext-grid>
+            </template>
+        </CardsPanel>
     </ext-dialog>
 </template>
 
 <script>
+import CardsPanel from "#src/components/cards.panel";
 import EntryModel from "./models/entry";
 
 export default {
+    "components": { CardsPanel },
+
     "props": {
         "methodId": {
             "type": String,
@@ -60,8 +69,6 @@ export default {
             // grid.setColumnMenu( null );
 
             this.grid.setStore( this.store );
-
-            this.reload();
         },
 
         async reload () {
