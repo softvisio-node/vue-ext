2bc76dbd94f8e6e70c3a85d27d749a8efd350f97
zdm <zdm@softvisio.net>
2023-04-23 17:59:01 +0300

    feat: administration component

--- src/components/user-tokens/panel.vue
+++ src/components/user-tokens/panel.vue
@@ -1,5 +1,5 @@
 <template>
-    <CardsPanel ref="cards" :store="store" @reload="reload" @storeLoad="_onStoreLoad">
+    <CardsPanel ref="cards" :store="store" @reload="reload" @render="reload" @storeLoad="_onStoreLoad">
         <template #items>
             <ext-toolbar docked="top">
                 <ext-searchfield :placeholder="i18nd(`vue-ext`, `Search tokens by name`)" width="200" @change="search"/>
@@ -52,8 +52,6 @@ export default {
             grid.setItemConfig( { "viewModel": true } );
 
             grid.setStore( this.store );
-
-            this.reload();
         },
 
         enabledColReady ( e ) {
