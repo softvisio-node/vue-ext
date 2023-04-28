45bc5ddea12bcd2a8d19e8f61e2efe2a900f4c94
zdm <zdm@softvisio.net>
2023-04-28 13:10:34 +0300

    feat: api-status

--- src/components/api-status/latest.panel.vue
+++ src/components/api-status/latest.panel.vue
@@ -184,45 +184,35 @@ export default {
                 am5 = cmp.am5;
 
             const chart = root.container.children.push( am5xy.XYChart.new( root, {
-                "layout": root.verticalLayout,
-                "panX": true,
-                "panY": true,
-                "pinchZoomX": true,
 
+                // "panX": true,
+                // "panY": true,
                 // "wheelX": "panX",
                 // "wheelY": "zoomX",
+                // "pinchZoomX": true,
             } ) );
 
             chart.set(
                 "cursor",
                 am5xy.XYCursor.new( root, {
-                    "behavior": "zoomX",
-                } )
-            );
-
-            // add scrollbar, https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
-            chart.set(
-                "scrollbarX",
-                am5.Scrollbar.new( root, {
-                    "orientation": "horizontal",
+                    "behavior": "none", // "zoomX",
                 } )
             );
 
             chart.children.unshift( am5.Label.new( root, {
-                "text": this.i18nd( "vue-ext", "Exceptions percent for the last 30 days (%)" ),
+                "text": this.i18nd( "vue-ext", "Exceptions percent for the last 60 minutes (%)" ),
                 "fontSize": 12,
                 "x": am5.percent( 50 ),
                 "centerX": am5.percent( 50 ),
             } ) );
 
             const xAxis = chart.xAxes.push( am5xy.DateAxis.new( root, {
-                "maxDeviation": 0,
                 "baseInterval": {
-                    "timeUnit": "hour",
+                    "timeUnit": "minute",
                     "count": 1,
                 },
                 "renderer": am5xy.AxisRendererX.new( root, {} ),
-                "tooltipDateFormat": "I",
+                "tooltipDateFormat": "HH:mm",
                 "tooltip": am5.Tooltip.new( root, {} ),
             } ) );
 
@@ -231,32 +221,18 @@ export default {
             } ) );
 
             const series1 = chart.series.push( am5xy.ColumnSeries.new( root, {
-                "name": "Exceptions",
+                "name": "Exceptions (%)",
                 "xAxis": xAxis,
                 "yAxis": yAxis,
-                "valueXField": "date",
                 "valueYField": "exceptions_percent",
-                "stroke": "red",
+                "valueXField": "date",
                 "fill": "red",
+                "stroke": "red",
                 "stacked": true,
                 "tooltip": am5.Tooltip.new( root, {
-                    "labelText": this.i18nd( "vue-ext", "Exceptions" ) + ": {valueY}%",
+                    "labelText": "Exceptions: {valueY}%",
                 } ),
             } ) );
-
-            // chart.series.push( am5xy.ColumnSeries.new( root, {
-            //     "name": "Declined",
-            //     "xAxis": xAxis,
-            //     "yAxis": yAxis,
-            //     "valueXField": "date",
-            //     "valueYField": "total_declined",
-            //     "fill": "red",
-            //     "stroke": "red",
-            //     "stacked": true,
-            //     "tooltip": am5.Tooltip.new( root, {
-            //         "labelText": "Declined requests: {valueY}",
-            //     } ),
-            // } ) );
 
             series1.data.processor = am5.DataProcessor.new( root, {
                 "dateFields": ["date"],
