cc56c109eb60de9cb5f007eac7b4f0bc30e5d2be
zdm <zdm@softvisio.net>
2023-05-14 16:18:09 +0300

    fix: notifications

--- src/components/notifications/panel.vue
+++ src/components/notifications/panel.vue
@@ -1,5 +1,5 @@
 <template>
-    <ext-tabpanel flex="1" layout='{"animation":{"direction":"vertical","type":"slide"}}' tabBar='{"defaults":{"flex":null,"height":"100"},"layout":{"align":"start","pack":"start","type":"vbox"}}' tabBarPosition="left">
+    <ext-tabpanel flex="1" layout='{"animation":{"direction":"vertical","type":"slide"}}' tabBar='{"defaults":{"flex":null,"height":"100"},"layout":{"align":"start","pack":"start","type":"vbox"}}' tabBarPosition="left" @ready="_ready">
         <!-- inbox             -->
         <ext-panel layout="fit" :title="i18nd(`vue-ext`, `Inbox`)">
             <InboxPanel/>
@@ -15,8 +15,31 @@
 <script>
 import InboxPanel from "#src/components/notifications/inbox.panel";
 import DonePanel from "#src/components/notifications/done.panel";
+import notificationsStore from "#src/components/notifications/store";
 
 export default {
     "components": { InboxPanel, DonePanel },
+
+    "computed": {
+        totalUndone () {
+            return notificationsStore.totalUndone;
+        },
+    },
+
+    "watch": {
+        "totalUndone": "_setNotificationsBadgeText",
+    },
+
+    "methods": {
+        _ready ( e ) {
+            this._inboxTab = e.detail.cmp.getTabBar().getTabs()[0];
+
+            this._setNotificationsBadgeText();
+        },
+
+        _setNotificationsBadgeText () {
+            this._inboxTab.setBadgeText( notificationsStore.totalUndone || "" );
+        },
+    },
 };
 </script>
