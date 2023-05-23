17d64bb89a1519b21633a25202d92264483fe330
zdm <zdm@softvisio.net>
2023-05-24 00:50:03 +0300

    fix: telegram

--- src/components/notifications/link-telegram.dialig.vue
+++ src/components/notifications/link-telegram.dialig.vue
@@ -1,10 +1,10 @@
 <template>
     <ext-dialog height="400" layout='{"align":"center","pack":"start","type":"vbox"}' :title="i18nd(`vue-ext`, `Link Telegram`)" width="350">
-        <ext-container :html='i18nd(`vue-ext`, `If you have Telegram installed on <b>THIS</b> device press button below and click "in Staty" Telegram window.`)' padding="0 0 10 0"/>
+        <ext-container :html='i18nd(`vue-ext`, `If you have Telegram installed on <b>THIS</b> device, then press button below to open Telegram bot and then click "Start":`)' padding="0 0 10 0"/>
 
         <ext-button :text="i18nd(`vue-ext`, `Link Telegram`)" ui="action" @tap="_openTelegramBot"/>
 
-        <ext-container :html="i18nd(`vue-ext`, `If you have Telegram installed on <b>OTHER</b> device - copy link url and open it on device there your Telegram is installed..`)" padding="50 0 10 0"/>
+        <ext-container :html="i18nd(`vue-ext`, `Or if you have Telegram installed on the <b>OTHER</b> device, then copy link url and open it on device there your Telegram is installed:`)" padding="50 0 10 0"/>
 
         <ext-button iconCls="fa-solid fa-copy" :text="i18nd(`vue-ext`, `Copy link`)" ui="action" @tap="_copyTelegramLink"/>
     </ext-dialog>
