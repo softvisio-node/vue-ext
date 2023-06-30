aab4a6e8ffc20623d007e8f62664e4ed8c4f16ef
zdm <zdm@softvisio.net>
2023-06-30 19:03:39 +0300

    feat: instance monitoring

--- src/components/development/monitoring/instances/models/instance.js
+++ src/components/development/monitoring/instances/models/instance.js
@@ -5,45 +5,15 @@ export default Ext.define( "", {
         "id",
 
         // fields
-        "component",
-        "method",
-        "calls",
-        "duration_per_call",
-        "exceptions",
-        "exceptions_per_call",
-        "calls_share",
-        "duration_share",
-        "exceptions_share",
-
-        // calculated
-        {
-            "name": "method_name",
-            "calculate": data => ( data.component + "/" + data.method ).replaceAll( /\/\/+/g, "/" ),
-        },
-
-        {
-            "name": "calls_text",
-            "calculate": data => data.calls + " / " + Ext.util.Format.percent( data.calls_share, "minimumFractionDigits:1,maximumFractionDigits:1" ),
-        },
-
-        {
-            "name": "duration_text",
-            "calculate": data => Ext.util.Format.percent( data.duration_share, "minimumFractionDigits:1,maximumFractionDigits:1" ),
-        },
-
-        {
-            "name": "duration_per_call_text",
-            "calculate": data => Ext.util.Format.number( data.duration_per_call, "minimumFractionDigits:1,maximumFractionDigits:1" ),
-        },
-
-        {
-            "name": "exceptions_text",
-            "calculate": data => data.exceptions + " / " + Ext.util.Format.percent( data.exceptions_share, "minimumFractionDigits:1,maximumFractionDigits:1" ),
-        },
-
-        {
-            "name": "exceptions_per_call_text",
-            "calculate": data => Ext.util.Format.percent( data.exceptions_per_call, "minimumFractionDigits:1,maximumFractionDigits:1" ),
-        },
+        "package",
+        "service",
+        "instance",
+        { "name": "created", "type": "date" },
+        { "name": "last_updated", "type": "date" },
+        "memory_total",
+        "memory_free",
+        "memory_rss",
+        "fs_total",
+        "fs_free",
     ],
 } );
