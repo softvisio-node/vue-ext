432c03856df5839fbd0136d1b21fffedb60659dd
zdm <zdm@softvisio.net>
2023-07-20 17:54:27 +0300

    fix: notifications profile

--- src/components/notifications/models/notification-type.js
+++ src/components/notifications/models/notification-type.js
@@ -13,5 +13,41 @@ export default Ext.define( "", {
 
         // calculated
         { "name": "title", "calculate": data => `<div class="notification-type-name">${app.locale.i18nd( "backend", data.name )}</div>${app.locale.i18nd( "backend", data.description )}` },
+
+        {
+            "name": "enabled",
+            "type": "bool",
+            calculate ( data ) {
+                for ( const channel of Object.values( data ) ) {
+                    if ( channel.enabled ) return true;
+                }
+
+                return false;
+            },
+        },
+
+        {
+            "name": "editable",
+            "type": "bool",
+            calculate ( data ) {
+                for ( const channel of Object.values( data ) ) {
+                    if ( channel.editable ) return true;
+                }
+
+                return false;
+            },
+        },
+
+        {
+            "name": "active",
+            "type": "bool",
+            calculate ( data ) {
+                for ( const channel of Object.values( data ) ) {
+                    if ( channel.active ) return true;
+                }
+
+                return false;
+            },
+        },
     ],
 } );
