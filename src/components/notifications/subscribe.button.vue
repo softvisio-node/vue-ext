3c790fc2fe691f206e5d568cd60288d16a66f673
zdm <zdm@softvisio.net>
2023-07-21 12:42:25 +0300

    fix: glob

---
+++ src/components/notifications/subscribe.button.vue
@@ -0,0 +1,65 @@
+<template>
+    <ext-container :hidden="_hidden" @ready="_ready">
+        <ext-button ref="button" iconCls="fa-regular fa-bell" margin="10 20 0 0" ui="action" width="55" @tap="showNotificationsDialog"/>
+
+        <NotificationsDialog ref="dialog"/>
+    </ext-container>
+</template>
+
+<script>
+import NotificationsDialog from "#src/components/notifications/dialog";
+
+export default {
+    "components": { NotificationsDialog },
+
+    "props": {
+        "hidden": {
+            "type": Boolean,
+            "default": false,
+        },
+    },
+
+    "computed": {
+        totalInbox () {
+            return this.$app.notifications.totalInbox;
+        },
+
+        _hidden () {
+            return !this.$app.notifications.internalNotificationsEnabled || this.hidden;
+        },
+    },
+
+    "watch": {
+        "totalInbox": "_setNotificationsBadgeText",
+    },
+
+    "methods": {
+        _ready ( e ) {
+            this.ext = e.detail.cmp;
+
+            this._setNotificationsBadgeText();
+        },
+
+        showNotificationsDialog () {
+            const dialog = this.$refs.dialog.ext;
+
+            // XXX fix for case, when .showBy() coordinates are incorrect on first show
+            if ( !this._initialized ) {
+                this._initialized = true;
+
+                dialog.showBy( this.ext, "tr-br" );
+                if ( dialog.activeAnimation ) dialog.activeAnimation.stop();
+                dialog.hide( false );
+            }
+
+            dialog.showBy( this.ext, "tr-tl" );
+
+            this.$app.notifications.refreshRelativeTime();
+        },
+
+        _setNotificationsBadgeText () {
+            this.$refs.button.ext.setBadgeText( this.$app.notifications.totalInbox || "" );
+        },
+    },
+};
+</script>
