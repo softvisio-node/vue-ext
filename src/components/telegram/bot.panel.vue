2bf7c5aa546b33e7b7cc793af09583c6e7d8bdc2
zdm <zdm@softvisio.net>
2023-08-21 12:29:11 +0300

    fix: telegram

---
+++ src/components/telegram/bot.panel.vue
@@ -0,0 +1,22 @@
+<template>
+    <ext-panel layut="hbox">
+        <BotInfoPanel flex="1" :telegramBotId="telegramBotId"/>
+        <BotStatsPanel flex="1" :telegramBotId="telegramBotId"/>
+    </ext-panel>
+</template>
+
+<script>
+import BotInfoPanel from "./bot-info.panel";
+import BotStatsPanel from "./bot-stats.panel";
+
+export default {
+    "components": { BotInfoPanel, BotStatsPanel },
+
+    "props": {
+        "telegramBotId": {
+            "type": String,
+            "required": true,
+        },
+    },
+};
+</script>
