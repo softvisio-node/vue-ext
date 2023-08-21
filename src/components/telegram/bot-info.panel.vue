1bb484e1fba3eadeb96c953efd3166c3e38a1ff3
zdm <zdm@softvisio.net>
2023-08-21 12:49:45 +0300

    fix: telegram

--- src/components/telegram/bot-info.panel.vue
+++ src/components/telegram/bot-info.panel.vue
@@ -10,23 +10,23 @@
                 <ext-panel defaults='{"labelAlign":"left","labelWidth":200}' padding="10 10 10 10" scrollable="true">
                     <ext-field bind="{record.name}" :label="l10n(`vue-ext`, `Bot name`)"/>
 
-                    <ext-field bind="{record.short_description}" :label="l10n(`vue-ext`, `Short description`)"/>
+                    <ext-displayfield bind="{record.telegram_username}" :label="l10n(`vue-ext`, `Telegram username`)"/>
+
+                    <ext-textareafield bind="{record.short_description}" :label="l10n(`vue-ext`, `Short description`)"/>
 
                     <ext-field bind="{record.description}" :label="l10n(`vue-ext`, `Description`)"/>
 
-                    <ext-field bind="{record.type}" :label="l10n(`vue-ext`, `Type`)"/>
+                    <ext-displayfield bind="{record.type}" :label="l10n(`vue-ext`, `Type`)"/>
 
-                    <ext-field bind="{record.static}" :label="l10n(`vue-ext`, `Static`)"/>
+                    <ext-displayfield bind="{record.static}" :label="l10n(`vue-ext`, `Static`)"/>
 
-                    <ext-field bind="{record.created}" :label="l10n(`vue-ext`, `Creation date`)"/>
+                    <ext-displayfield bind="{record.created}" :label="l10n(`vue-ext`, `Creation date`)"/>
 
-                    <ext-field bind="{record.telegram_username}" :label="l10n(`vue-ext`, `Telegram username`)"/>
-
-                    <ext-field bind="{record.total_users}" :label="l10n(`vue-ext`, `Total users`)"/>
-                    <ext-field bind="{record.total_subscribed_users}" :label="l10n(`vue-ext`, `Total subscribed users`)"/>
-                    <ext-field bind="{record.total_unsubscribed_users}" :label="l10n(`vue-ext`, `Total unsubscribed users`)"/>
-                    <ext-field bind="{record.total_returned_users}" :label="l10n(`vue-ext`, `Total returned users`)"/>
-                    <ext-field bind="{record.total_banned_users}" :label="l10n(`vue-ext`, `Total banned users`)"/>
+                    <ext-displayfield bind="{record.total_users}" :label="l10n(`vue-ext`, `Total users`)"/>
+                    <ext-displayfield bind="{record.total_subscribed_users}" :label="l10n(`vue-ext`, `Total subscribed users`)"/>
+                    <ext-displayfield bind="{record.total_unsubscribed_users}" :label="l10n(`vue-ext`, `Total unsubscribed users`)"/>
+                    <ext-displayfield bind="{record.total_returned_users}" :label="l10n(`vue-ext`, `Total returned users`)"/>
+                    <ext-displayfield bind="{record.total_banned_users}" :label="l10n(`vue-ext`, `Total banned users`)"/>
                 </ext-panel>
             </ext-panel>
         </template>
