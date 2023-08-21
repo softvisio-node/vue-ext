814b3a9701060da2d22db36bd4fa2fd350ab2bbd
zdm <zdm@softvisio.net>
2023-08-21 11:34:40 +0300

    fix: telegram

--- src/components/telegram/panel.vue
+++ src/components/telegram/panel.vue
@@ -9,7 +9,27 @@
 
         <template #data>
             <ext-panel ref="dataPanel" layout="fit" viewModel="true">
-                <ext-container bind="{record.name}"/>
+                <ext-panel defaults='{"labelAlign":"left","labelWidth":200}' padding="10 10 10 10">
+                    <ext-field bind="{record.name}" :label="l10n(`vue-ext`, `Bot name`)"/>
+
+                    <ext-field bind="{record.short_description}" :label="l10n(`vue-ext`, `Short description`)"/>
+
+                    <ext-field bind="{record.description}" :label="l10n(`vue-ext`, `Description`)"/>
+
+                    <ext-field bind="{record.type}" :label="l10n(`vue-ext`, `Type`)"/>
+
+                    <ext-field bind="{record.static}" :label="l10n(`vue-ext`, `Static`)"/>
+
+                    <ext-field bind="{record.created}" :label="l10n(`vue-ext`, `Creation date`)"/>
+
+                    <ext-field bind="{record.telegram_username}" :label="l10n(`vue-ext`, `Telegram username`)"/>
+
+                    <ext-field bind="{record.total_users}" :label="l10n(`vue-ext`, `Total users`)"/>
+                    <ext-field bind="{record.total_subscribed_users}" :label="l10n(`vue-ext`, `Total subscribed users`)"/>
+                    <ext-field bind="{record.total_unsubscribed_users}" :label="l10n(`vue-ext`, `Total unsubscribed users`)"/>
+                    <ext-field bind="{record.total_returned_users}" :label="l10n(`vue-ext`, `Total returned users`)"/>
+                    <ext-field bind="{record.total_banned_users}" :label="l10n(`vue-ext`, `Total banned users`)"/>
+                </ext-panel>
             </ext-panel>
         </template>
     </CardsPanel>
