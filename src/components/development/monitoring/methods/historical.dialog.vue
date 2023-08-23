c2300d738acf96786435a633f0be9d590ad7e888
zdm <zdm@softvisio.net>
2023-08-23 09:36:10 +0300

    fix: telegram

--- src/components/development/monitoring/methods/historical.dialog.vue
+++ src/components/development/monitoring/methods/historical.dialog.vue
@@ -45,31 +45,19 @@ export default {
             const root = cmp.root,
                 am5 = cmp.am5;
 
+            // chart
             const chart = root.container.children.push( am5xy.XYChart.new( root, {
                 "layout": root.verticalLayout,
                 "panX": true,
-                "panY": true,
-                "pinchZoomX": true,
 
                 // "wheelX": "panX",
+                "pinchZoomX": true,
+
+                // "panY": true,
                 // "wheelY": "zoomX",
             } ) );
 
-            chart.set(
-                "cursor",
-                am5xy.XYCursor.new( root, {
-                    "behavior": "zoomX",
-                } )
-            );
-
-            // add scrollbar, https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
-            chart.set(
-                "scrollbarX",
-                am5.Scrollbar.new( root, {
-                    "orientation": "horizontal",
-                } )
-            );
-
+            // title
             chart.children.push( am5.Label.new( root, {
                 "text": this.l10nd( "vue-ext", "Calls for the last 30 days" ),
                 "fontSize": 12,
@@ -77,6 +65,7 @@ export default {
                 "centerX": am5.percent( 50 ),
             } ) );
 
+            // x axis
             const xAxis = chart.xAxes.push( am5xy.DateAxis.new( root, {
                 "maxDeviation": 0,
                 "baseInterval": {
@@ -88,49 +77,46 @@ export default {
                 "tooltip": am5.Tooltip.new( root, {} ),
             } ) );
 
+            // y axis
             const yAxis = chart.yAxes.push( am5xy.ValueAxis.new( root, {
                 "renderer": am5xy.AxisRendererY.new( root, {} ),
             } ) );
 
+            // series 1
             const series1 = chart.series.push( am5xy.ColumnSeries.new( root, {
                 "name": "Calls",
-                "xAxis": xAxis,
-                "yAxis": yAxis,
+                xAxis,
+                yAxis,
                 "valueXField": "date",
                 "valueYField": "calls",
-                "stroke": "green",
-                "fill": "green",
-                "stacked": true,
                 "tooltip": am5.Tooltip.new( root, {
                     "labelText": this.l10nd( "vue-ext", "Calls" ) + ": {valueY}",
                 } ),
+                "stroke": am5.color( "#00ff00" ),
+                "fill": am5.color( "#00ff00" ),
             } ) );
 
-            // chart.series.push( am5xy.ColumnSeries.new( root, {
-            //     "name": "Declined",
-            //     "xAxis": xAxis,
-            //     "yAxis": yAxis,
-            //     "valueXField": "date",
-            //     "valueYField": "total_declined",
-            //     "fill": "red",
-            //     "stroke": "red",
-            //     "stacked": true,
-            //     "tooltip": am5.Tooltip.new( root, {
-            //         "labelText": "Declined requests: {valueY}",
-            //     } ),
-            // } ) );
-
+            // data processor
             series1.data.processor = am5.DataProcessor.new( root, {
                 "dateFields": ["date"],
                 "dateFormat": "i",
             } );
 
-            // const legend = chart.children.push( am5.Legend.new( root, {
-            //     "x": am5.percent( 50 ),
-            //     "centerX": am5.percent( 50 ),
-            // } ) );
+            // scroll bar
+            chart.set(
+                "scrollbarX",
+                am5.Scrollbar.new( root, {
+                    "orientation": "horizontal",
+                } )
+            );
 
-            // legend.data.setAll( chart.series.values );
+            // curdor
+            chart.set(
+                "cursor",
+                am5xy.XYCursor.new( root, {
+                    "behavior": "zoomX",
+                } )
+            );
         },
 
         _createDurationChart ( cmp ) {
