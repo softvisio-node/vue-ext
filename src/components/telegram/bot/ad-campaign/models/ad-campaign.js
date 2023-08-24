cc8a8f914db314aa5d8e2628b176154aed44236e
zdm <zdm@softvisio.net>
2023-08-24 08:03:24 +0300

    fix: telegram

---
+++ src/components/telegram/bot/ad-campaign/models/ad-campaign.js
@@ -0,0 +1,33 @@
+export default Ext.define( "", {
+    "extend": "Ext.data.Model",
+
+    "fields": [
+        { "name": "id", "type": "string" },
+
+        // fields
+        "type",
+        { "name": "static", "type": "bool" },
+        "locales",
+        { "name": "created", "type": "date" },
+
+        "name",
+        "short_description",
+        "description",
+
+        "telegram_id",
+        "telegram_username",
+        { "name": "telegram_can_join_groups", "type": "bool" },
+        { "name": "telegram_can_read_all_group_messages", "type": "bool" },
+        { "name": "telegram_supports_inline_queries", "type": "bool" },
+
+        "total_users",
+        "total_subscribed_users",
+        "total_unsubscribed_users",
+        "total_returned_users",
+        "total_banned_users",
+
+        { "name": "started", "type": "bool" },
+        { "name": "error", "type": "bool" },
+        "error_text",
+    ],
+} );
