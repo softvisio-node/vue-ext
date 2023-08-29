7e7c09c0ec3e91625b6ed0760ee323214e59cb87
zdm <zdm@softvisio.net>
2023-08-30 00:22:39 +0300

    fix: telegram

--- src/components/telegram/bot/panel.vue
+++ src/components/telegram/bot/panel.vue
@@ -1,7 +1,7 @@
 <template>
     <ext-panel layout="hbox">
         <BotInfoPanel flex="1" :telegramBotId="telegramBotId"/>
-        <BotStatsPanel flex="1" :telegramBotId="telegramBotId"/>
+        <BotStatsPanel flex="1" resizable='{"edges":"west","snap":200,"split":true}' scrollable="true" :telegramBotId="telegramBotId"/>
     </ext-panel>
 </template>
 
