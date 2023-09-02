ef780f2072d062f2d9071dab93a910faa674f734
zdm <zdm@softvisio.net>
2023-09-03 00:56:06 +0300

    fix: telegram

--- src/components/telegram/bot/ad-campaign/models/ad-campaign.js
+++ src/components/telegram/bot/ad-campaign/models/ad-campaign.js
@@ -11,6 +11,13 @@ export default Ext.define( "", {
         "callbacl",
         "telegram_bot_username",
 
+        { "name": "last_user_created", "type": "data" },
+        "total_users",
+        "total_subscribed_users",
+        "total_unsubscribed_users",
+        "total_returned_users",
+        "total_banned_users",
+
         // calculated
         { "name": "url", "calculate": data => `https://t.ne/${data.telegram_bot_username}?start=${data.callback}` },
     ],
