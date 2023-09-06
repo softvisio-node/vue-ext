bffcca1c125b9ddfaf35509505d624ab2db2ddfe
zdm <zdm@softvisio.net>
2023-09-06 13:47:20 +0300

    fix: monitoring

---
+++ src/components/development/monitoring/methods/charts.dialog.vue
@@ -0,0 +1,28 @@
+<template>
+    <ext-dialog height="90%" layout="fit" scrollable="true" :title="title" viewModel="true" width="90%">
+        <CharstPanel period="30 days" :record="record"/>
+    </ext-dialog>
+</template>
+
+<script>
+import CharstPanel from "./charts.panel";
+
+export default {
+    "components": { CharstPanel },
+
+    "props": {
+        "record": {
+            "type": Object,
+            "required": true,
+        },
+    },
+
+    "computed": {
+        title () {
+            return this.l10nd( "vue-ext", `Method stats` );
+        },
+    },
+
+    "methods": {},
+};
+</script>
