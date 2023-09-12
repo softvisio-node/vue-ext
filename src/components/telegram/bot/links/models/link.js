e28dd2db175ab20bf3a0cbace350c2e70fd46d39
zdm <zdm@softvisio.net>
2023-09-12 13:12:04 +0300

    fix: mount promise

--- src/components/telegram/bot/links/models/link.js
+++ src/components/telegram/bot/links/models/link.js
@@ -1,19 +1,15 @@
 export default Ext.define( "", {
     "extend": "Ext.data.Model",
 
+    "proxy": {
+        "api": {
+            "read": "telegram/bots/lunks/read",
+        },
+    },
+
     "fields": [
         { "name": "id", "type": "string" },
 
         // fields
-        "nme",
-        "description",
-        { "name": "created", "type": "date" },
-        "url",
-        { "name": "last_user_created", "type": "data" },
-        "total_users",
-        "total_subscribed_users",
-        "total_unsubscribed_users",
-        "total_returned_users",
-        "total_banned_users",
     ],
 } );
