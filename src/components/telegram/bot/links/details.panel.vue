c16d39c47cdd78a308c5d2108d8dcd65bb6b48e8
zdm <zdm@softvisio.net>
2023-09-15 18:14:22 +0300

    fix: telegram

--- src/components/telegram/bot/links/details.panel.vue
+++ src/components/telegram/bot/links/details.panel.vue
@@ -1,10 +1,10 @@
 <template>
-    <ext-tabpanel flex="1" layout='{"animation":{"direction":"vertical","type":"slide"}}' margin="1 0 0  0" tabBar='{"defaults":{"flex":null,"height":150,"textAlign":"center","width":40},"layout":{"align":"start","pack":"start","type":"vbox"}}' tabBarPosition="left" tabRotation1="none" viewModel="true">
+    <ext-tabpanel layout='{"animation":{"direction":"vertical","type":"slide"}}' tabBar='{"defaults":{"flex":null,"height":150,"textAlign":"center"},"layout":{"align":"start","pack":"start","type":"vbox"}}' tabBarPosition="left" tabRotation1="none" viewModel="true">
         <ext-panel layout="fit" :title="l10n(`Info`)">
             <BotLinkInfoPanel flex="1" :telegramBotLinkRecord="telegramBotLinkRecord"/>
         </ext-panel>
 
-        <ext-panel layout="fit" :title="l10n(`Charts`)">
+        <ext-panel iconAlign="left" iconCls="fa-solid fa-chart-line" layout="fit" :title="l10n(`Charts`)">
             <BotLinkStatsPanel flex="1" scrollable="true" :telegramBotLinkRecord="telegramBotLinkRecord"/>
         </ext-panel>
     </ext-tabpanel>
