31cfc69bce8c6bc4af6b83275d961e63aba80a67
zdm <zdm@softvisio.net>
2023-09-21 10:52:27 +0300

    fix: telegram

--- src/components/telegram/bot/users/models/user.js
+++ src/components/telegram/bot/users/models/user.js
@@ -1,4 +1,4 @@
-import app from "#app";
+// import app from "#app";
 
 export default Ext.define( "", {
     "extend": "Ext.data.Model",
@@ -13,99 +13,15 @@ export default Ext.define( "", {
         { "name": "id", "type": "string" },
 
         // fields
-        "name",
-        "description",
-        "link",
+        { "name": "is_bot", "type": "bool" },
+        "username",
+        "first_name",
+        "last_name",
+        "phone",
         { "name": "created", "type": "date" },
-        { "name": "last_user_created", "type": "date" },
-
-        "total_users",
-        "total_subscribed_users",
-        "total_unsubscribed_users",
-        "total_returned_users",
-        "total_banned_users",
-
-        // calculated
-        { "name": "created_text", "calculate": data => app.locale.formatDate( data.created, "dateStyle:short,timeStyle:short" ) },
-        { "name": "created_relative", "calculate": data => app.locale.formatRelativeDate( data.created ) },
-        { "name": "created_html", "calculate": data => `${data.created_text} (${data.created_relative})` },
-
-        { "name": "last_user_created_text", "calculate": data => ( data.last_user_created ? app.locale.formatDate( data.created, "dateStyle:short,timeStyle:short" ) : "--" ) },
-        { "name": "last_user_created_relative", "calculate": data => ( data.last_user_created ? app.locale.formatRelativeDate( data.created ) : "--" ) },
-        {
-            "name": "last_user_created_html",
-            calculate ( data ) {
-                if ( data.last_user_created ) {
-                    return `${data.created_text} (${data.created_relative})`;
-                }
-                else {
-                    return "--";
-                }
-            },
-        },
-
-        {
-            "name": "total_subscribed_users_percent",
-            calculate ( data ) {
-                if ( data.total_users ) {
-                    return app.locale.formatNumber( data.total_subscribed_users / data.total_users );
-                }
-                else {
-                    return 0;
-                }
-            },
-        },
-
-        { "name": "total_subscribed_users_percent_text", "calculate": data => app.locale.formatPercent( data.total_subscribed_users_percent ) },
-
-        { "name": "total_subscribed_users_text", "calculate": data => `${data.total_subscribed_users} (${data.total_subscribed_users_percent_text})` },
-
-        {
-            "name": "total_unsubscribed_users_percent",
-            calculate ( data ) {
-                if ( data.total_users ) {
-                    return app.locale.formatNumber( data.total_unsubscribed_users / data.total_users );
-                }
-                else {
-                    return 0;
-                }
-            },
-        },
-
-        { "name": "total_unsubscribed_users_percent_text", "calculate": data => app.locale.formatPercent( data.total_unsubscribed_users_percent ) },
-
-        { "name": "total_unsubscribed_users_text", "calculate": data => `${data.total_unsubscribed_users} (${data.total_unsubscribed_users_percent_text})` },
-
-        {
-            "name": "total_returned_users_percent",
-            calculate ( data ) {
-                if ( data.total_users ) {
-                    return app.locale.formatNumber( data.total_returned_users / data.total_users );
-                }
-                else {
-                    return 0;
-                }
-            },
-        },
-
-        { "name": "total_returned_users_percent_text", "calculate": data => app.locale.formatPercent( data.total_returned_users_percent ) },
-
-        { "name": "total_returned_users_text", "calculate": data => `${data.total_returned_users} (${data.total_returned_users_percent_text})` },
-
-        {
-            "name": "total_banned_users_percent",
-            calculate ( data ) {
-                if ( data.total_users ) {
-                    return app.locale.formatNumber( data.total_banned_users / data.total_users );
-                }
-                else {
-                    return 0;
-                }
-            },
-        },
-
-        { "name": "total_banned_users_percent_text", "calculate": data => app.locale.formatPercent( data.total_banned_users_percent ) },
-
-        { "name": "total_banned_users_text", "calculate": data => `${data.total_banned_users} (${data.total_banned_users_percent_text})` },
+        { "name": "last_activity", "type": "date" },
+        { "name": "subscribed", "type": "bool" },
+        { "name": "returned", "type": "bool" },
+        { "name": "banned", "type": "bool" },
     ],
 } );
