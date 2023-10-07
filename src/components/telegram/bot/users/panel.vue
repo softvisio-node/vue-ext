92c0848f259bcdd6e259da1c877bc1ed88fce848
zdm <zdm@softvisio.net>
2023-10-07 11:59:36 +0300

    fix: telegram

--- src/components/telegram/bot/users/panel.vue
+++ src/components/telegram/bot/users/panel.vue
@@ -3,13 +3,13 @@
         <CardsPanel ref="cardsPanel" :store="store" @refresh="refresh">
             <template #docked>
                 <ext-toolbar docked="top">
-                    <ext-searchfield :placeholder="l10n(`Search links`)" width="150" @change="_search"/>
+                    <ext-searchfield :placeholder="l10n(`Search users`)" width="150" @change="_search"/>
 
                     <ext-spacer/>
 
                     <ext-button iconCls="fa-solid fa-redo" :text="l10n(`Refresh`)" @tap="refresh"/>
 
-                    <ext-button iconCls="fa-solid fa-table-columns" pressed="true" :tooltip="l10n(`Toggle link details panel`)" @tap="toggleLinkDetailsPanel"/>
+                    <ext-button iconCls="fa-solid fa-table-columns" pressed="true" :tooltip="l10n(`Toggle user details panel`)" @tap="toggleDetailsPanel"/>
                 </ext-toolbar>
             </template>
 
@@ -22,15 +22,13 @@
                     <ext-column dataIndex="full_name" flex="1" sorter='{"property":"first_name"}' :text="l10n(`Name`)"/>
 
                     <ext-column dataIndex="phone" flex="1" :text="l10n(`Phone`)"/>
-
-                    <!-- <ext-column width="150" @ready="_actionColReady"/> -->
                 </ext-grid>
             </template>
         </CardsPanel>
 
         <!-- details panel -->
-        <ext-panel ref="linkDetailsPanel" docked="right" layout="fit" minWidth="400" resizable='{"edges":"west","snap":200,"split":true}' width="400">
-            <DetailsPanel :telegramBotLinkRecord="telegramBotLinkRecord" @linkDelete="_onLinkDelete"/>
+        <ext-panel ref="detailsPanel" docked="right" layout="fit" minWidth="400" resizable='{"edges":"west","snap":200,"split":true}' width="400">
+            <DetailsPanel :telegramBotUserRecord="telegramBotUserRecord"/>
         </ext-panel>
     </ext-panel>
 </template>
@@ -54,7 +52,7 @@ export default {
 
     data () {
         return {
-            "telegramBotLinkRecord": null,
+            "telegramBotUserRecord": null,
         };
     },
 
@@ -93,7 +91,7 @@ export default {
         _gridReady ( e ) {
             const cmp = e.detail.cmp;
 
-            cmp.on( "select", ( grid, selection ) => ( this.telegramBotLinkRecord = selection[0] ) );
+            cmp.on( "select", ( grid, selection ) => ( this.telegramBotUserRecord = selection[0] ) );
 
             cmp.setStore( this.store );
         },
@@ -106,26 +104,6 @@ export default {
                 "widget": {
                     "xtype": "avatar",
                     "bind": "{record.avatar_url}",
-                },
-            } );
-        },
-
-        _actionColReady ( e ) {
-            const cmp = e.detail.cmp;
-
-            cmp.setCell( {
-                "xtype": "widgetcell",
-                "widget": {
-                    "xtype": "container",
-                    "layout": { "type": "hbox", "pack": "end", "align": "center" },
-                    "items": [
-                        {
-                            "xtype": "button",
-                            "iconCls": "fa-regular fa-copy",
-                            "text": this.l10n( "Copy link" ),
-                            "handler": this._copyLink.bind( this ),
-                        },
-                    ],
                 },
             } );
         },
@@ -145,32 +123,18 @@ export default {
             }
         },
 
-        async _copyLink ( button ) {
-            const record = button.up( "gridrow" ).getRecord();
-
-            this.$utils.copyToClipboard( record.get( "link" ) );
-
-            this.$toast( this.l10n( "Link copied to the clipboard" ) );
-        },
-
-        toggleLinkDetailsPanel ( e ) {
+        toggleDetailsPanel ( e ) {
             const button = e.detail.sender,
                 pressed = button.getPressed();
 
             button.setPressed( !pressed );
 
             if ( !pressed ) {
-                this.$refs.linkDetailsPanel.ext.show();
+                this.$refs.detailsPanel.ext.show();
             }
             else {
-                this.$refs.linkDetailsPanel.ext.hide();
+                this.$refs.detailsPanel.ext.hide();
             }
-        },
-
-        _onLinkDelete ( telegramBotLinkrecord ) {
-            this.telegramBotLinkRecord = null;
-
-            this.refresh();
         },
     },
 };
