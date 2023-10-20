b0276f883d58f7bb5ac94b50e172d3cb0d37a728
zdm <zdm@softvisio.net>
2023-10-20 20:34:12 +0300

    fix: telegram

--- src/components/telegram/create-bot.dialog.vue
+++ src/components/telegram/create-bot.dialog.vue
@@ -1,32 +1,20 @@
 <template>
-    <ext-dialog height="98%" layout="fit" :title="title" width="95%" @ready="_ready"/>
+    <ext-dialog height="500" layout="fit" :title="l10n(`Create Telegram bot`)" width="500">
+        <ext-panel ref="cardPanel" layout="card">
+            <ext-fieldpanel>
+                <ext-textfield :label="l10n(`Enter your Telegram bot API key`)"/>
+
+                <ext-toolbar docked="bottom">
+                    <ext-spacer/>
+                    <ext-button :text="l10n(`Next`)" ui="action"/>
+                </ext-toolbar>
+            </ext-fieldpanel>
+        </ext-panel>
+    </ext-dialog>
 </template>
 
 <script>
 export default {
-    "props": {
-        "panel": {
-            "type": Object,
-            "required": true,
-        },
-        "telegramBotId": {
-            "type": String,
-            "required": true,
-        },
-        "title": {
-            "type": String,
-            "required": true,
-        },
-    },
-
-    "methods": {
-        async _ready ( e ) {
-            await this.$mount( this.panel, {
-                "props": {
-                    "telegramBotId": this.telegramBotId,
-                },
-            } );
-        },
-    },
+    "methods": {},
 };
 </script>
