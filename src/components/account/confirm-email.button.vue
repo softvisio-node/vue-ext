373ccc8ad866b04239a52c9a3ec14c9f7f27e140
zdm <zdm@softvisio.net>
2023-10-23 19:34:45 +0300

    fix: email confirmation

---
+++ src/components/account/confirm-email.button.vue
@@ -0,0 +1,53 @@
+<template>
+    <ext-button ref="burron" :hidden="emailConfirmed" :text="l10n(`Confirm email`)" ui="decline" @tap="_confirmEmail"/>
+</template>
+
+<script>
+import Events from "#core/events";
+
+export default {
+    "computed": {
+        emailConfirmed () {
+            return this.$app.user.isEmailConfirmed;
+        },
+    },
+
+    mounted () {
+        this._events = new Events().link( this.$api );
+
+        this._events.on( "session/email/confirm", () => this.$app.user.setEmailConfirmed( true ) );
+    },
+
+    unmounted () {
+        this._events.unlinkAll();
+
+        this._events = null;
+    },
+
+    "methods": {
+        async _confirmEmail () {
+            const button = this.$refs.button.ext;
+
+            button.disable();
+
+            const res = await this.$api.call( "session/send-confirmation-email" );
+
+            button.enable();
+
+            if ( res.ok ) {
+                if ( res.data.email_confirmed ) {
+                    this.$app.user.setEmailConfirmed( true );
+
+                    this.$toast( this.l10n( `Email address confirmed` ) );
+                }
+                else {
+                    this.$toast( this.l10n( `Confirmation email sent` ) );
+                }
+            }
+            else {
+                this.$toast( res );
+            }
+        },
+    },
+};
+</script>
