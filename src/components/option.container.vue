917b9656edf2f801c4ef6f80c79516a61f51ba8a
zdm <zdm@softvisio.net>
2023-10-24 12:18:54 +0300

    fix: components

---
+++ src/components/option.container.vue
@@ -0,0 +1,31 @@
+<template>
+    <ext-container layout='{"align":"start","type":"hbox"}'>
+        <ext-container layout='{"type":"vbox"}' :width="lableWidth">
+            <ext-container :html="label" style="font-size: 1.3em"/>
+            <ext-container :html="description"/>
+        </ext-container>
+
+        <ext-container flex="1">
+            <slot name="option"/>
+        </ext-container>
+    </ext-container>
+</template>
+
+<script>
+export default {
+    "props": {
+        "label": {
+            "type": String,
+            "required": true,
+        },
+        "description": {
+            "type": String,
+            "default": "",
+        },
+        "labelWidth": {
+            "type": Number,
+            "default": 100,
+        },
+    },
+};
+</script>
