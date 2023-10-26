03c133fa4d327f59a63416ed8d9b7c7082c71b73
zdm <zdm@softvisio.net>
2023-10-26 20:14:53 +0300

    fix: actopn token

--- src/components/notifications/link-telegram.dialig.vue
+++ src/components/notifications/link-telegram.dialig.vue
@@ -1,10 +1,12 @@
 <template>
-    <ext-dialog height="350" layout='{"align":"center","pack":"start","type":"vbox"}' :title="l10n(`Link Telegram`)" width="300">
-        <ext-container :html='l10n(`If you have Telegram installed on <b>THIS</b> device, then press button below to open Telegram bot and then click "Start":`)' padding="0 0 10 0"/>
+    <ext-dialog height="400" layout='{"align":"center","pack":"start","type":"vbox"}' :title="l10n(`Link Telegram`)" width="300">
+        <ext-container :html='l10n(`If you have Telegram installed on <b>THIS</b> device, then press button below to open Telegram bot and then click "Start":`)'/>
 
         <ext-button :text="l10n(`Link Telegram`)" ui="action" @tap="_openTelegramBot"/>
 
-        <ext-container :html="l10n(`Or if you have Telegram installed on the <b>OTHER</b> device, then copy link url and open it on device there your Telegram is installed:`)" padding="50 0 10 0"/>
+        <ext-container height="30"/>
+
+        <ext-container :html="l10n(`Or if you have Telegram installed on the <b>OTHER</b> device, then copy link url and open it on device there your Telegram is installed:`)"/>
 
         <ext-button iconCls="fa-solid fa-copy" :text="l10n(`Copy link`)" ui="action" @tap="_copyTelegramLink"/>
     </ext-dialog>
