7163044098d5e75ea4d44ef389bfee40ff02e20e
zdm <zdm@softvisio.net>
2023-10-30 08:01:45 +0200

    fix: telegram link

--- src/components/telegram/bot/component.js
+++ src/components/telegram/bot/component.js
@@ -3,8 +3,6 @@ import TelegramComponentModel from "./models/component";
 const REGISTRY = {};
 
 export default class TelegramBotComponent {
-    static #store;
-
     constructor () {
         if ( REGISTRY[this.id] ) {
             throw Error( `Telegram bot id ${this.id} already registered` );
@@ -19,21 +17,17 @@ export default class TelegramBotComponent {
     }
 
     static get store () {
-        if ( !this.#store ) {
-            this.#store = Ext.create( "Ext.data.Store", {
-                "remoteSort": false,
-                "remoteFilter": false,
-                "data": Object.values( REGISTRY ).map( component => {
-                    return new TelegramComponentModel( {
-                        "id": component.id,
-                        "name": component.name,
-                        "description": component.description,
-                    } );
-                } ),
-            } );
-        }
-
-        return this.#store;
+        return Ext.create( "Ext.data.Store", {
+            "remoteSort": false,
+            "remoteFilter": false,
+            "data": Object.values( REGISTRY ).map( component => {
+                return new TelegramComponentModel( {
+                    "id": component.id,
+                    "name": component.name,
+                    "description": component.description,
+                } );
+            } ),
+        } );
     }
 
     // propertirs
