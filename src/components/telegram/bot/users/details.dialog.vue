5fc3e8bbf5667bbad43d1b87f7f601023e49e367
zdm <zdm@softvisio.net>
2023-11-10 06:30:41 +0200

    fix: telegram

---
+++ src/components/telegram/bot/users/details.dialog.vue
@@ -0,0 +1,28 @@
+<template>
+    <ext-dialog height="90%" layout="fit" scrollable="true" :title="title" width="600">
+        <DetailsPanel :telegramBotUserRecord="telegramBotUserRecord"/>
+    </ext-dialog>
+</template>
+
+<script>
+import DetailsPanel from "./details.panel";
+
+export default {
+    "components": { DetailsPanel },
+
+    "props": {
+        "telegramBotUserRecord": {
+            "type": Object,
+            "required": true,
+        },
+    },
+
+    "computed": {
+        title () {
+            return this.l10n( `Telegram bot user` ) + ": " + this.telegramBotUserRecord.get( "name" );
+        },
+    },
+
+    "methods": {},
+};
+</script>
