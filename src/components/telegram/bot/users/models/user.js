5279f4c755be49433e4b44aabe98551ab5653606
zdm <zdm@softvisio.net>
2023-11-20 23:59:05 +0200

    fix: telegram

--- src/components/telegram/bot/users/models/user.js
+++ src/components/telegram/bot/users/models/user.js
@@ -14,6 +14,7 @@ export default Ext.define( null, {
 
         // fields
         "telegram_bot_id",
+        "telegram_user_id",
         "avatar_url",
         { "name": "is_bot", "type": "bool" },
         "username",
