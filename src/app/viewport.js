98388e3c54509b875497e85d740e0378dcd4aa0f
zdm <zdm@softvisio.net>
2024-01-20 13:53:57 +0200

    fix: exp app

--- src/app/viewport.js
+++ src/app/viewport.js
@@ -20,11 +20,12 @@ export default class VueExtViewport extends VueViewport {
 
         await super.init();
 
+        window[Symbol.for( "app" )] = this.app;
+
         return new Promise( resolve => {
             Ext.application( {
                 "name": "-",
                 "quickTips": true,
-                "app": this.app,
                 "launch": resolve,
             } );
         } );
