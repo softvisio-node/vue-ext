c0beedba23ce218d7703a5f2f529d1e6fba56097
zdm <zdm@softvisio.net>
2024-01-27 12:39:03 +0200

    fix: permissions

--- src/components/acl/models/permission.js
+++ src/components/acl/models/permission.js
@@ -1,10 +1,12 @@
+import app from "#app";
+
 export default Ext.define( null, {
     "extend": "Ext.data.Model",
 
     "fields": [
 
         // fields
-        { "name": "permissions", "convert": value => new Set( value ) },
+        { "name": "permissions", "convert": value => app.user.permissions.set( value ) },
 
         // calculatte
         { "name": "create", "calculate": data => data.permissions.has( "acl:create" ) },
