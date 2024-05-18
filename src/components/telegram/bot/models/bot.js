1c460b096d43ddafa4ff4c3a1a2b4b0350499791
zdm <zdm@softvisio.net>
2024-05-18 09:42:09 +0300

    fix: telegram bot stats percent

--- src/components/telegram/bot/models/bot.js
+++ src/components/telegram/bot/models/bot.js
@@ -67,20 +67,20 @@ export default Ext.define( null, {
         },
 
         // subscribed
-        { "name": "total_subscribed_users_percent", "calculate": data => data.total_subscribed_users / data.total_users },
+        { "name": "total_subscribed_users_percent", "calculate": data => ( data.total_users ? data.total_subscribed_users / data.total_users : 0 ) },
         { "name": "total_subscribed_users_percent_text", "calculate": data => app.locale.formatPercent( data.total_subscribed_users_percent ) },
 
         { "name": "total_subscribed_users_text", "calculate": data => `${ data.total_subscribed_users } (${ data.total_subscribed_users_percent_text })` },
 
         // unsubscribed
-        { "name": "total_unsubscribed_users_percent", "calculate": data => data.total_unsubscribed_users / data.total_users },
+        { "name": "total_unsubscribed_users_percent", "calculate": data => ( data.total_users ? data.total_unsubscribed_users / data.total_users : 0 ) },
 
         { "name": "total_unsubscribed_users_percent_text", "calculate": data => app.locale.formatPercent( data.total_unsubscribed_users_percent ) },
 
         { "name": "total_unsubscribed_users_text", "calculate": data => `${ data.total_unsubscribed_users } (${ data.total_unsubscribed_users_percent_text })` },
 
         // banned
-        { "name": "total_banned_users_percent", "calculate": data => data.total_banned_users / data.total_users },
+        { "name": "total_banned_users_percent", "calculate": data => ( data.total_users ? data.total_banned_users / data.total_users : 0 ) },
 
         { "name": "total_banned_users_percent_text", "calculate": data => app.locale.formatPercent( data.total_banned_users_percent ) },
 
